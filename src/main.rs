612f7587fbc4a8975650b40651b2e628dc801b66
the 8th mage <eee34353@gmail.com>
2020-12-04 13:21:06 +0200

    advent 3

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 fn main() {
     let input = include_str!("input.txt");
-//     let input = "1-3 a: abcde
-// 1-3 b: cdefg
-// 2-9 c: ccccccccc";
+    //     let input = "1-3 a: abcde
+    // 1-3 b: cdefg
+    // 2-9 c: ccccccccc";
     // let values = input;
     // .map(|str| str.split_terminator('-').collect())
     // println!("{}", input);
-    let inputLines= input.lines();
-    let mut countValid = 0;
-    let mut countValid2 = 0;
-    for line in inputLines{
-        let values:Vec<&str> = line.split_whitespace().map(|str| str.trim_matches(':')).collect();
-        let policy:Vec<i32> = values[0].split_terminator('-').map(|str| str.parse::<i32>().unwrap()).collect();
-        let check = values[1].chars().next().unwrap();;
-        let count = values[2].chars().filter(|&c| c==check).count();
-        if count >= policy[0] as usize && count <= policy[1] as usize {
-            countValid = countValid+1;
-        }
-        let first = values[2].chars().nth(policy[0] as usize -1).unwrap() == check;
-        let second = values[2].chars().nth(policy[1] as usize -1).unwrap() == check;
-        if (first && !second) || (second && !first) {
-            countValid2 = countValid2 +1;
+    let inputLines = input.lines();
+    let mut map: Vec<Vec<char>> = inputLines.map(|str| str.chars().collect()).collect();
+    let mut xPos = 0;
+    let mut countValid = [0, 0, 0, 0, 0];
+    let xSlopes = [1, 3, 5, 7, 1];
+    let ySlopes = [1, 1, 1, 1, 2];
+    let mut yPos = 0;
+    for slope in 0..xSlopes.len() {
+        xPos = 0;
+        yPos = 0;
+        while (yPos < map.len()) {
+            if map[yPos][xPos] == '#' {
+                countValid[slope] += 1;
+            }
+            xPos += xSlopes[slope];
+            yPos += ySlopes[slope];
+            xPos %= map[0].len();
         }
     }
-
-    println!("{}", countValid);
-    println!("{}", countValid2);
+    let mut mul = 1usize;
+    for x in &countValid {
+        mul = mul * x;
+    }
+    println!("{}", mul);
+    // for line in inputLines{
+    // map.
+    // }
 }
