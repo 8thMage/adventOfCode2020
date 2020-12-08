e6f18264271be65330c735e68ab886eea5db5cf7
the 8th mage <eee34353@gmail.com>
2020-12-08 19:40:29 +0200

    advent 7

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,88 @@
+use std::collections::HashMap;
 fn main() {
     let input = include_str!("input.txt");
-//     let input = "abc
+//     let input = 
+// "shiny gold bags contain 2 dark red bags.
+// dark red bags contain 2 dark orange bags.
+// dark orange bags contain 2 dark yellow bags.
+// dark yellow bags contain 2 dark green bags.
+// dark green bags contain 2 dark blue bags.
+// dark blue bags contain 2 dark violet bags.
+// dark violet bags contain no other bags.";
+    let dependendt = input
+        .lines()
+        .map(|rule: &str| {
+            let first = rule.split_terminator("contain").next().unwrap();
+            let color = first.split_terminator(" bags").next().unwrap();
 
-// a
-// b
-// c
+            let second = rule.split_terminator("contain").skip(1).next().unwrap();
+            let deps = second
+                .split_terminator(",")
+                .map(|specific_bag: &str| {
+                    if specific_bag.contains("no") {
+                        None
+                    } else {
+                        let number = i64::from_str_radix(
+                            specific_bag.split_whitespace().next().unwrap(),
+                            10,
+                        );
+                        let color = specific_bag
+                            .trim_left_matches(|c| c <= '9' && c >= '0' || c == ' ')
+                            .split_terminator(" bag")
+                            .next();
+                        Some((number.unwrap(), color.unwrap()))
+                    }
+                })
+                .collect::<Vec<Option<(i64, &str)>>>();
+            (color, deps)
+        }).collect::<std::collections::HashMap<&str, Vec<Option<(i64, &str)>>>>();
+    let mut checks: std::collections::hash_set::HashSet<&str> =
+        std::collections::hash_set::HashSet::new();
+    checks.insert("shiny gold");
+    findContains(&dependendt, &mut checks);
+    let mut countPerBag = HashMap::new();
+    let sum = findNumber(&dependendt, &mut countPerBag,  "shiny gold");
 
-// ab
-// ac
+    println!("{}", checks.len() - 1);
+    println!("sum {} ", sum);
+}
 
-// a
-// a
-// a
-// a
+fn findContains<'a>(dependents: &'a HashMap<&str, Vec<Option<(i64, &str)>>>,
+    checks :&mut std::collections::HashSet<&'a str>) {
+    for deps in dependents {
+        if checks.contains(deps.0) {
+            continue;
+        }
+        for dep in deps.1 {
+            if let Some(x) = dep {
+                if checks.contains(x.1) {
+                    checks.insert(deps.0);
+                    findContains(dependents, checks);
+                    break;
+                }
+            }
+        }
+    } 
+}
 
-// b";
-    let groups = input.split_terminator("\r\n\r\n");
-    // let groups = input.split_terminator("\n\n");
-    let first = groups.fold(0u32, |acc, group| {
-        let sum = group.split_whitespace().fold(0u32, |acc, s| acc | s.chars().fold(0u32, |acc, c| acc | 1 << (c as usize - 'a' as usize)));
-        acc + sum.count_ones()
-    });
-    let groups = input.split_terminator("\r\n\r\n");
-    let second = groups.fold(0u32, |acc, group| {
-        let sum = group.split_whitespace().fold(u32::max_value(), |acc, s| acc & s.chars().fold(0u32, |acc, c| acc | 1 << (c as usize - 'a' as usize)));
-        acc + sum.count_ones()
-    });
-    println!("{}", first);
-    println!("{}", second);
+fn findNumber<'a>(
+    dependents: &'a HashMap<&str, Vec<Option<(i64, &str)>>>,
+    countPerBag: &mut HashMap<&'a str, i64>,
+    specificColor:&'a str,
+) -> i64 {
+    if countPerBag.contains_key(specificColor) {
+        return countPerBag[specificColor];
+    }
+    let mut sum = 0;
+    for dep in &dependents[specificColor] {
+        if dep.is_none() {
+            let cloned = specificColor.clone();
+            countPerBag.insert(specificColor, 0);
+            return 0;
+        } else {
+            sum += dep.unwrap().0 * (1 + findNumber(dependents, countPerBag, dep.unwrap().1));
+        }
+    }
+    countPerBag.insert(specificColor, sum);
+    sum
 }
