d01c4397de3bc00b3063cd539b3f98099bacca3a
the 8th mage <eee34353@gmail.com>
2020-12-12 18:14:43 +0200

    advent 11

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,104 @@
 use std::collections::HashMap;
 fn main() {
     let input = include_str!("input.txt");
-//         let input =
-// "16
-// 10
-// 15
-// 5
-// 1
-// 11
-// 7
-// 19
-// 6
-// 12
-// 4";
-    let difference = input
+//     let input = 
+// "L.LL.LL.LL
+// LLLLLLL.LL
+// L.L.L..L..
+// LLLL.LL.LL
+// L.LL.LL.LL
+// L.LLLLL.LL
+// ..L.L.....
+// LLLLLLLLLL
+// L.LLLLLL.L
+// L.LLLLL.LL";
+    let mut map: Vec<Vec<(bool, bool)>> = input
         .lines()
-        .map(|s| s.parse::<i32>().unwrap())
-        .max()
-        .unwrap()
-        + 3;
-    let mut sorted_input: Vec<i32> = input
-        .lines()
-        .map(|s| s.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-    sorted_input.push(0);
-    sorted_input.push(difference);
-    sorted_input.sort();
-    let mut count_one = 0;
-    let mut count_three = 0;
-    for x in 0..sorted_input.len() - 1 {
-        if sorted_input[x] == sorted_input[x + 1] - 1 {
-            count_one += 1;
-        } else if sorted_input[x] != sorted_input[x + 1] - 2 {
-            count_three += 1;
-        }
-    }
-    println!("{} {} {}", count_one, count_three, count_one * count_three);
-    println!("{}", number_of_connections(&sorted_input, &mut HashMap::new()));
+        .map(|s| {
+            s.chars()
+                .map(|c| match c {
+                    'L' => (true, false),
+                    '#' => (true, true),
+                    '.' => (false, false),
+                    _ => (false, false),
+                })
+                .collect()
+        })
+        .collect();
+    let mut newMap: Vec<Vec<(bool, bool)>> = Vec::new();
+    newMap.resize(map.len(), {
+        let mut v = Vec::new();
+        v.resize(map[0].len(), (false, false));
+        v
+    });
+    let tmp = map.to_vec();
+    let mut stable = false;
+    while !stable {
+        stable = true;
+        // println!("");
+        for y in 0..map.len() {
+            for x in 0..map[0].len() {
+                // print!(
+                //     "{}",
+                //     match map[y][x] {
+                //         (false, false) => '.',
+                //         (true, false) => 'L',
+                //         (true, true) => '#',
+                //         (false, true) => panic!(),
+                //     }
+                // );
+                if map[y][x].0 {
+                    let mut count = 0;
+                    for dirY in -1i32..2 {
+                        for dirX in -1..2 {
+                            if dirY != 0 || dirX != 0 {
+                                let mut newY = y as i32 + dirY;
+                                let mut newX = x as i32 + dirX;
 
-}
+                                while (newX >= 0
+                                    && newY >= 0
+                                    && newY < map.len() as i32
+                                    && newX < map[0].len() as i32
+                                    && !map[newY as usize][newX as usize].0)
+                                {
+                                    newX += dirX;
+                                    newY += dirY;
+                                }
+                                if (newX >= 0
+                                    && newY >= 0
+                                    && newY < map.len() as i32
+                                    && newX < map[0].len() as i32)
+                                {
+                                    count += map[newY as usize][newX as usize].1 as i32;
+                                }
+                            }
+                        }
+                    }
+                    // let (dirX,dirY) = (0,0);
 
-fn number_of_connections(
-    sorted_input: &[i32],
-    ran_table: &mut HashMap<i32, i64>,
-) -> i64 {
-    if sorted_input.len() == 1 {
-        return 1;
-    }
-    if ran_table.contains_key(&sorted_input[0]) {
-        return ran_table[&sorted_input[0]];
-    } else {
-        let mut count = 0;
-        for i in 1..4.min((sorted_input.len()) as i32) {
-            if sorted_input[i as usize] < sorted_input[0] + 4 {
-                count += number_of_connections(&sorted_input[i as usize..], ran_table);
+                    // for newY in y.saturating_sub(1)..(y + 2).min(map.len()) {
+                    //     for newX in x.saturating_sub(1)..(x + 2).min(map[0].len()) {
+                    //         if newX != x || newY != y {
+                    //             count += map[newY][newX].1 as i32;
+                    //         }
+                    //     }
+                    // }
+                    if (!map[y][x].1 && count == 0) || (map[y][x].1 && count < 5) {
+                        newMap[y][x] = (true, true);
+                    } else {
+                        newMap[y][x] = (true, false);
+                    }
+                }
+                stable = stable & (newMap[y][x].1 == map[y][x].1);
             }
+            // println!("");
         }
-        ran_table.insert(sorted_input[0], count);
-        return count;
+        let tmp = map.to_vec();
+        map = newMap.to_vec();
     }
+    let res = map
+        .iter()
+        .map(|v| v.iter().filter(|(a, b)| *b).count())
+        .sum::<usize>();
+    println!("{}", res);
 }
