533563bf13c30887a948587f91009d4c1329e13c
the 8th mage <eee34353@gmail.com>
2020-12-15 15:05:34 +0200

    advent 13

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,75 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, thread::current};
 fn main() {
     let input = include_str!("input.txt");
-//     let input = 
-// "F10
-// N3
-// F7
-// R90
-// F11";
-let mut heading = (10, 1);
-let (mut x,mut y) = (0,0); 
-for instruction in input.lines() {
-    let command = instruction.chars().next().unwrap();
-    let mut number = instruction.chars().skip(1).collect::<String>().parse::<i64>().unwrap();
-    match command {
-        'N' => heading.1 += number,
-        'S' => heading.1 -= number,
-        'E' => heading.0 += number,
-        'W' => heading.0 -= number,
-        'F' => { 
-            x += heading.0 * number;
-            y += heading.1 * number;
-        },
-        'R' => while number > 0 {
-            let (newx,newy) = (heading.1, -heading.0);
-            heading = (newx, newy);
-            number -= 90;
-        } 
-        'L' => while number > 0 {
-            let (newx,newy) = (-heading.1, heading.0);
-            heading = (newx, newy);
-            number -= 90;
-        } 
-        _ => panic!(),
+//     let input = "939
+// 7,13,x,x,59,x,31,19";
+// let input = "939
+// 17,x,13,19";
 
+let x = input.lines().next().unwrap().parse::<usize>().unwrap();
+    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
+    let res = busses.map(|bus| if let Ok(bus_number) = bus.parse::<usize>() {
+        (bus_number - x % bus_number, bus_number)
+    } else {
+        (usize::MAX, 1)
+    }
+    ).min().unwrap();
+    println!("{} {} {}", res.0, res.1, res.0 * res.1);
+    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
+    let mut bussesFine = busses.map(|s| s.parse::<usize>().is_ok()).collect::<Vec<bool>>();    
+    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
+    let mut bussesSorted = busses.map(|s| s.parse::<usize>()).filter(|b|b.is_ok()).map(|b|b.unwrap()).collect::<Vec<usize>>();    
+    let all_prime = bussesSorted.iter().all(|b| is_prime(*b));
+    let mut current_gcd = bussesSorted[0] as i128;
+    let mut current_number = 0i128;
+    let (a, b) = euqclide(11, 7);
+    println!("{} {}", a,b);
+    let mut count = 0;
+    let mut iter = bussesFine.iter().enumerate().peekable();
+    for i in 1..bussesSorted.len() as isize {
+        iter.next();
+        while (!*iter.peek().unwrap().1) {
+            iter.next();
+        };
+        let p = current_number as i128;
+        let q = bussesSorted[i as usize] as i128;
+        let (a, b) = euqclide(current_gcd, q);
+        let a = a.rem_euclid(current_gcd * q as i128);
+        let b = b.rem_euclid(current_gcd * q as i128);
+        println!("{:x?} {:x?}", current_number,current_gcd);
+        let n = (q  - iter.peek().unwrap().0 as i128).rem_euclid(current_gcd * q as i128) as i128;
+        let half1 = (a * current_gcd).rem_euclid(current_gcd * q as i128) * n;
+        let half2 = (current_number * b).rem_euclid(current_gcd * q as i128) * q;
+        current_number =  (half1 + half2).rem_euclid(current_gcd * q as i128);
+        current_gcd =current_gcd * q;
+    }
+    print!("{}", current_number);
+}
+
+fn euqclide(a:i128, b:i128) -> (i128, i128) {
+    if (a < b) {
+        let (x, y) = euqclide(b, a);
+        return (y,x);
+    }
+    let r = a % b;
+    let q = a / b;
+    if (r == 1) {
+        return (r as i128, - (q as i128));
+    } 
+    else {
+        let (x, y) = euqclide(b, r);
+        return (y as i128, x - y as i128 * q as i128);
     }
 }
-println!("{}", x.abs() + y.abs());
+
+fn is_prime(x:usize)->bool {
+    for i in 2..x {
+        if(i * i > x) {
+            return true;
+        } 
+        if x % i == 0 {
+            return false;
+        }
+    }
+    return true;
 }
