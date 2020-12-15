e4490af20067ef285d2e0334fe09b31bc410f845
the 8th mage <eee34353@gmail.com>
2020-12-15 17:07:14 +0200

    advent 14

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,90 @@
-use std::{collections::HashMap, thread::current};
+use std::{
+    collections::{HashMap, HashSet},
+    mem::zeroed,
+    thread::current,
+};
 fn main() {
     let input = include_str!("input.txt");
-//     let input = "939
-// 7,13,x,x,59,x,31,19";
-// let input = "939
-// 17,x,13,19";
+//     let input = "mask = 000000000000000000000000000000X1001X
+// mem[42] = 100
+// mask = 00000000000000000000000000000000X0XX
+// mem[26] = 1";
+    let mut zero_mask = usize::MAX;
+    let mut one_mask = 0;
+    println!("{} {}", one_mask, zero_mask);
+    let mut hash = HashMap::new();
+    let mut floatingX = HashSet::new();
+    floatingX.insert(0);
 
-let x = input.lines().next().unwrap().parse::<usize>().unwrap();
-    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
-    let res = busses.map(|bus| if let Ok(bus_number) = bus.parse::<usize>() {
-        (bus_number - x % bus_number, bus_number)
-    } else {
-        (usize::MAX, 1)
-    }
-    ).min().unwrap();
-    println!("{} {} {}", res.0, res.1, res.0 * res.1);
-    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
-    let mut bussesFine = busses.map(|s| s.parse::<usize>().is_ok()).collect::<Vec<bool>>();    
-    let busses = input.lines().skip(1).next().unwrap().split_terminator(",");
-    let mut bussesSorted = busses.map(|s| s.parse::<usize>()).filter(|b|b.is_ok()).map(|b|b.unwrap()).collect::<Vec<usize>>();    
-    let all_prime = bussesSorted.iter().all(|b| is_prime(*b));
-    let mut current_gcd = bussesSorted[0] as i128;
-    let mut current_number = 0i128;
-    let (a, b) = euqclide(11, 7);
-    println!("{} {}", a,b);
-    let mut count = 0;
-    let mut iter = bussesFine.iter().enumerate().peekable();
-    for i in 1..bussesSorted.len() as isize {
-        iter.next();
-        while (!*iter.peek().unwrap().1) {
-            iter.next();
-        };
-        let p = current_number as i128;
-        let q = bussesSorted[i as usize] as i128;
-        let (a, b) = euqclide(current_gcd, q);
-        let a = a.rem_euclid(current_gcd * q as i128);
-        let b = b.rem_euclid(current_gcd * q as i128);
-        println!("{:x?} {:x?}", current_number,current_gcd);
-        let n = (q  - iter.peek().unwrap().0 as i128).rem_euclid(current_gcd * q as i128) as i128;
-        let half1 = (a * current_gcd).rem_euclid(current_gcd * q as i128) * n;
-        let half2 = (current_number * b).rem_euclid(current_gcd * q as i128) * q;
-        current_number =  (half1 + half2).rem_euclid(current_gcd * q as i128);
-        current_gcd =current_gcd * q;
-    }
-    print!("{}", current_number);
-}
-
-fn euqclide(a:i128, b:i128) -> (i128, i128) {
-    if (a < b) {
-        let (x, y) = euqclide(b, a);
-        return (y,x);
-    }
-    let r = a % b;
-    let q = a / b;
-    if (r == 1) {
-        return (r as i128, - (q as i128));
-    } 
-    else {
-        let (x, y) = euqclide(b, r);
-        return (y as i128, x - y as i128 * q as i128);
-    }
-}
-
-fn is_prime(x:usize)->bool {
-    for i in 2..x {
-        if(i * i > x) {
-            return true;
-        } 
-        if x % i == 0 {
-            return false;
+    for line in input.lines() {
+        let subLine = &line[0..3];
+        match subLine {
+            "mas" => {
+                one_mask = line
+                    .chars()
+                    .skip(7)
+                    .fold(0usize, |acc, c| (acc << 1) | ((c == '1') as usize));
+                zero_mask = line
+                    .chars()
+                    .skip(7)
+                    .fold(0usize, |acc, c| (acc << 1) | ((c != '0') as usize));
+                floatingX = line
+                    .chars()
+                    .skip(7)
+                    .enumerate()
+                    .filter_map(|(ind, c)| if (c == 'X') { Some(ind) } else { None })
+                    .collect();
+                // currentFloating.insert(0);
+                // for c in line.chars().skip(7) {
+                //     let mut newFloating = HashSet::new();
+                //     for flt in currentFloating {
+                //         newFloating.insert(flt << 1);
+                //         if (c == 'X') {
+                //             newFloating.insert((flt<<1)|1);
+                //         }
+                //     }
+                //     currentFloating = newFloating;
+                // }
+                // floatingX = currentFloating;
+            }
+            "mem" => {
+                // let pos:usize = line.strip_prefix("mem[").unwrap().split("]").next().unwrap().parse().unwrap();
+                // let value:usize = line.split("= ").skip(1).next().unwrap().parse().unwrap();
+                // let newValue = (value | one_mask) &(zero_mask);
+                // hash.insert( pos, newValue );
+                let pos: usize = line
+                    .strip_prefix("mem[")
+                    .unwrap()
+                    .split("]")
+                    .next()
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let value: usize = line.split("= ").skip(1).next().unwrap().parse().unwrap();
+                let new_pos = pos | one_mask;
+                let mut currentFloating = HashSet::new();
+                currentFloating.insert(0);
+                for bit in 0..36 {
+                    let mut newFloating = HashSet::new();
+                    for flt in &currentFloating {
+                        if floatingX.contains(&bit) {
+                            newFloating.insert(flt << 1);
+                            newFloating.insert((flt << 1) | 1);
+                        } else {
+                            newFloating.insert(flt << 1 | ((new_pos>>(35 - bit))&1));
+                        }
+                    }
+                    currentFloating = newFloating;
+                }
+                for &flt in &currentFloating {
+                    hash.insert(flt, value);
+                    // println!("{:b} {}", flt, flt);
+                }
+            }
+            _ => {
+                panic!();
+            }
         }
     }
-    return true;
+    println!("{}", hash.iter().fold(0, |acc, (_, val2)| acc + val2));
 }
