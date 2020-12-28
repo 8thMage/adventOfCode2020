53403b2c26d19eafb408125042357e907d011477
the 8th mage <eee34353@gmail.com>
2020-12-28 20:12:27 +0200

    advent 23

--- src/main.rs
+++ src/main.rs
@@ -3,97 +3,59 @@ use std::collections::{btree_map::Iter, HashMap, HashSet, VecDeque};
 fn main() {
     let start_time = std::time::Instant::now();
     let input = include_str!("input.txt");
-    let mut players = input.split_terminator("\n\n");
-    let mut player_one = players
-        .next()
-        .unwrap()
-        .lines()
-        .skip(1)
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<std::collections::VecDeque<usize>>();
-    let mut player_two = players
-        .next()
-        .unwrap()
-        .lines()
-        .skip(1)
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<std::collections::VecDeque<usize>>();
-    recursiveGame(&mut player_one, &mut player_two);
-    // while player_one.len() != 0 && player_two.len() != 0 {
-    //     let p1 = player_one.pop_front().unwrap();
-    //     let p2 = player_two.pop_front().unwrap();
-    //     if p1 < p2 {
-    //         player_two.push_back(p2);
-    //         player_two.push_back(p1);
-    //     } else {
-    //         player_one.push_back(p1);
-    //         player_one.push_back(p2);
-    //     }
-    // }
-    let player = {
-        if player_one.len() == 0 {
-            player_two
-        } else {
-            player_one
+    let time = std::time::Instant::now();
+    for len in 1000000+1..1000000+2 {
+        // let len = 1 << loglen;
+        let mut circle = Vec::new();
+        let mut proto_circle: Vec<usize> = input.chars().map(|c| c as usize - '0' as usize).collect();
+        for v in proto_circle.len() + 1..len {
+            proto_circle.push(v);
         }
-    };
-    let score = player
-        .iter()
-        .rev()
-        .enumerate()
-        .fold(0, |acc, (index, p)| acc + (index + 1) * p);
-    println!("{}", score);
-    for p in player {
-    }
-}
+        circle.resize(len, 0);
+        for i in 0..proto_circle.len() {
+            circle[proto_circle[i]] = proto_circle[(i + 1) % proto_circle.len()];
+        }
+        let mut current_cup_index = proto_circle[0];
+        for turn in 0..10000000 {
+            let current_cup = current_cup_index;
+            let mut new_current_cup = current_cup;
+            let mut found = [true, false, false, false, false]; 
+            for _ in 1..4 {
+                new_current_cup = circle[new_current_cup];
+                if (current_cup + len - 1 - new_current_cup) % (len - 1) <= 3 {
+                    found[(current_cup + len - 1 - new_current_cup) % (len - 1)] = true;
+                }
+            }
+            new_current_cup = (current_cup + len - 1 - found.iter().position(|c| !c).unwrap()) % (len - 1);
+            if new_current_cup == 0 {
+                new_current_cup = len - 1;
+            }
+            let t = circle[current_cup];
+            let f = circle[new_current_cup];
+            let k = circle[circle[circle[current_cup]]];
+            circle[current_cup] = circle[k];
+            circle[new_current_cup] = t;
+            circle[k] = f;
+            current_cup_index = circle[current_cup];
+            // if (turn % 100 == 0) {
+              //     let onePos = (circle.iter().position(|&c| c == 1).unwrap() + 1)% circle.len();
+              //     let onePosPastOne = (circle.iter().position(|&c| c == 1).unwrap() + 2)% circle.len();
+              //     // println!("{} {} {}", turn, circle[onePos], circle[onePosPastOne]);
+              // }
+        }
+        let mut current_cup_index = 1;
+        // for pos in 0..len - 2 {
+        //     // let onePos = (circle.iter().position(|&c| c == 1).unwrap() + 1) % circle.len();
+        //     // let onePosPastOne = (circle.iter().position(|&c| c == 1).unwrap() + 2) % circle.len();
+        //     current_cup_index = circle[current_cup_index];
+        //     print!("{}", current_cup_index);
+        // }
+        // println!("");
+        let newTime = std::time::Instant::now();
+        println!("{}", newTime.duration_since(time).as_millis());
+        println!("{}", circle[1]);
+        println!("{}", circle[circle[1]]);
+        println!("{}", circle[1] * circle[circle[1]]);
 
-fn recursiveGame(
-    player_one: &mut VecDeque<usize>,
-    player_two: &mut VecDeque<usize>,
-) -> bool {
-    let mut control = HashSet::new();
-    while player_one.len() != 0 && player_two.len() != 0 {
-        let mut c = "".to_string();
-        for p in player_one.iter() {
-            c = c + " " + &p.to_string();
-        }
-        c = c + "|";
-        for p in player_two.iter() {
-            c = c + " " + &p.to_string();
-        }
-        if control.contains(&c) {
-            return false;
-        } 
-        control.insert(c);
-        let p1 = player_one.pop_front().unwrap();
-        let p2 = player_two.pop_front().unwrap();
-        let mut win = p1 < p2;
-        let len1 = player_one.len();
-        let len2 = player_two.len();
-        if p1 <= player_one.len() && p2 <= player_two.len() {
-            let mut newPlayerOne = player_one
-                .iter()
-                .cloned()
-                .take(p1)
-                .collect::<VecDeque<usize>>();
-            let mut newPlayerTwo = player_two
-                .iter()
-                .cloned()
-                .take(p2)
-                .collect::<VecDeque<usize>>();
-            win = recursiveGame(&mut newPlayerOne, &mut newPlayerTwo);
-        }
-        if win {
-            player_two.push_back(p2);
-            player_two.push_back(p1);
-        } else {
-            player_one.push_back(p1);
-            player_one.push_back(p2);
-        }
-    }
-    if player_one.len() == 0 {
-        return true;
-    } else {
-        return false;
     }
 }
