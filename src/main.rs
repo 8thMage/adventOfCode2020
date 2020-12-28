dbbcee70378eb8615ab579b690147b402ad2f6ab
the 8th mage <eee34353@gmail.com>
2020-12-28 20:50:32 +0200

    advent 24

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,77 @@ fn main() {
     let start_time = std::time::Instant::now();
     let input = include_str!("input.txt");
     let time = std::time::Instant::now();
-    for len in 1000000+1..1000000+2 {
-        // let len = 1 << loglen;
-        let mut circle = Vec::new();
-        let mut proto_circle: Vec<usize> = input.chars().map(|c| c as usize - '0' as usize).collect();
-        for v in proto_circle.len() + 1..len {
-            proto_circle.push(v);
+    let mut coordinates = (0,0); 
+    let mut flipped:HashSet<(isize, isize)> = HashSet::new();
+    for line in input.lines() {
+        coordinates = (0,0); 
+        let chars_vector:Vec::<char> = line.chars().collect();
+        let mut start = 0;
+        while start < chars_vector.len() {
+            if chars_vector[start] == 's' {
+                coordinates.1 -= 1;
+                if chars_vector[start + 1] == 'e' {
+                    coordinates.0 += 1;
+                }
+                start += 2;
+                continue;
+            }
+            if chars_vector[start] == 'n' {
+                coordinates.1 += 1;
+                if chars_vector[start + 1] == 'w' {
+                    coordinates.0 -= 1;
+                }
+                start += 2;
+                continue;
+            }
+            if chars_vector[start] == 'w' {
+                coordinates.0 -= 1
+            }
+            if chars_vector[start] == 'e' {
+                coordinates.0 += 1;
+            }
+            start += 1;
         }
-        circle.resize(len, 0);
-        for i in 0..proto_circle.len() {
-            circle[proto_circle[i]] = proto_circle[(i + 1) % proto_circle.len()];
+        if flipped.contains(&(coordinates)) {
+            flipped.remove(&(coordinates));
+        } else {
+            flipped.insert(coordinates);
         }
-        let mut current_cup_index = proto_circle[0];
-        for turn in 0..10000000 {
-            let current_cup = current_cup_index;
-            let mut new_current_cup = current_cup;
-            let mut found = [true, false, false, false, false]; 
-            for _ in 1..4 {
-                new_current_cup = circle[new_current_cup];
-                if (current_cup + len - 1 - new_current_cup) % (len - 1) <= 3 {
-                    found[(current_cup + len - 1 - new_current_cup) % (len - 1)] = true;
+    }
+    println!("{}", flipped.len());
+
+    for turn in 0..100 {
+        let mut new_flipped = HashSet::new();
+        let mut maxX = isize::MIN;
+        let mut maxY = isize::MIN;
+        let mut minX = isize::MAX;
+        let mut minY = isize::MAX;
+        for v in &flipped {
+            maxX = maxX.max(v.0);
+            minX = minX.min(v.0);
+            maxY = maxY.max(v.1);
+            minY = minY.min(v.1);
+        }
+        for x in minX-1..maxX + 2 {
+            for y in minY-1..maxY + 2 {
+                let mut countBlack = 0;
+                countBlack += flipped.contains(&(x - 1, y)) as isize;
+                countBlack += flipped.contains(&(x + 1, y)) as isize;
+                countBlack += flipped.contains(&(x - 1, y + 1)) as isize;
+                countBlack += flipped.contains(&(x + 1, y - 1)) as isize;
+                countBlack += flipped.contains(&(x, y + 1)) as isize;
+                countBlack += flipped.contains(&(x, y - 1)) as isize;
+                let c = flipped.contains(&(x, y));
+                if c && countBlack > 0 && countBlack <= 2 {
+                    new_flipped.insert((x, y));
+                } else if !c && countBlack == 2 {
+                    new_flipped.insert((x, y));
                 }
             }
-            new_current_cup = (current_cup + len - 1 - found.iter().position(|c| !c).unwrap()) % (len - 1);
-            if new_current_cup == 0 {
-                new_current_cup = len - 1;
-            }
-            let t = circle[current_cup];
-            let f = circle[new_current_cup];
-            let k = circle[circle[circle[current_cup]]];
-            circle[current_cup] = circle[k];
-            circle[new_current_cup] = t;
-            circle[k] = f;
-            current_cup_index = circle[current_cup];
-            // if (turn % 100 == 0) {
-              //     let onePos = (circle.iter().position(|&c| c == 1).unwrap() + 1)% circle.len();
-              //     let onePosPastOne = (circle.iter().position(|&c| c == 1).unwrap() + 2)% circle.len();
-              //     // println!("{} {} {}", turn, circle[onePos], circle[onePosPastOne]);
-              // }
         }
-        let mut current_cup_index = 1;
-        // for pos in 0..len - 2 {
-        //     // let onePos = (circle.iter().position(|&c| c == 1).unwrap() + 1) % circle.len();
-        //     // let onePosPastOne = (circle.iter().position(|&c| c == 1).unwrap() + 2) % circle.len();
-        //     current_cup_index = circle[current_cup_index];
-        //     print!("{}", current_cup_index);
-        // }
-        // println!("");
-        let newTime = std::time::Instant::now();
-        println!("{}", newTime.duration_since(time).as_millis());
-        println!("{}", circle[1]);
-        println!("{}", circle[circle[1]]);
-        println!("{}", circle[1] * circle[circle[1]]);
+
+        flipped = new_flipped;
+        println!("{}", flipped.len());
 
     }
 }
