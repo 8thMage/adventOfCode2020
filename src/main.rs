19d0093bfd3f7dd4e1b5bccee020a5d666f736a3
the 8th mage <eee34353@gmail.com>
2021-01-08 12:35:20 +0200

    euler 4

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use core::panic;
 fn main() {
-    let mut newNum:i64 = 600851475143;
-    'i: for i in 2..newNum - 1 {
-        if i% 1000 == 0 {
-            // println!("{}", i);
+  let mut max = 0;
+  for i in 100..1000 {
+      for j in 100..1000 {
+        if isPalindrome(i * j) {
+            max = (i * j).max(max);
         }
-        if newNum % i == 0 {
-            for j in 2..i {
-                if i % j == 0 {
-                    continue 'i;
-                }
-            }
-            while newNum % i ==0 {
-                newNum = newNum / i;
-            }
-            println!("{}", i);
+      }
+  }
+  println!("{}", max);
+}
+
+fn isPalindrome(num:i32) -> bool {
+    let mut stack = std::collections::vec_deque::VecDeque::new();
+    let mut new_num = num;
+    while new_num > 0 {
+        stack.push_front(new_num % 10);
+        new_num = new_num / 10;
+    } 
+    while stack.len() > 1 {
+        if stack.pop_back() != stack.pop_front() {
+            return false;
         }
     }
+    true
 }
