e3a2c8d67b1597886685aededb0d1652f4f6eb22
the 8th mage <eee34353@gmail.com>
2021-01-08 12:14:51 +0200

    project euler 2

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,11 @@
 use core::panic;
-use std::collections::{btree_map::Iter, HashMap, HashSet, VecDeque};
 fn main() {
-    let start_time = std::time::Instant::now();
-    let input = include_str!("input.txt");
-    let mut subject = 7isize;
-    let mut value = 1isize;
-    let cardTarget = 13135480;
-    let mut card_loop_size = 0;
-    while cardTarget != value {
-        value = value * subject;
-        value = value % 20201227;
-        card_loop_size +=1;
+    let mut fib = Vec::new();
+    fib.push(1);
+    fib.push(1);
+    while fib.last().unwrap() <= &4000000 {
+        fib.push(fib.last().unwrap() + fib[fib.len() - 2]);
     }
-    println!("{}", card_loop_size);
-    value = 1;
-    let doorTarget = 8821721;
-    let mut door_loop_size = 0;
-    while doorTarget != value {
-        value = value * subject;
-        value = value % 20201227;
-        door_loop_size +=1;
-    }
-    println!("{}", door_loop_size);
-    value = 1;
-    subject = doorTarget;
-    for _ in 0..card_loop_size {
-        value = value * subject;
-        value = value % 20201227;
-    }
-    println!("{}", value);
-    value = 1;
-    subject = cardTarget;
-
-    for _ in 0..door_loop_size {
-        value = value * subject;
-        value = value % 20201227;
-    }
-    println!("{}", value);
-
+    let sum:i32 = fib.iter().filter(|&x| x % 2 == 0).sum();
+    println!("{}", sum);
 }
