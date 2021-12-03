ad33b054a268ef2c1e39da157c2df396752ba23b
the 8th mage <eee34353@gmail.com>
2021-12-03 23:55:08 +0200

    advent 2021 1

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,13 @@ use std::collections::*;
 mod helpers;
 use helpers::*;
 fn main() {
-    let time = std::time::Instant::now();
-    'i: for i in 1..100000 {
-        let firstLen = digits(i).iter().cloned().collect::<HashSet<usize>>();
-        let twoLen = digits(2 * i).iter().cloned().collect::<HashSet00000<usize>>();
-        let threeLen = digits(3 * i)
-            .iter()
-            .cloned()
-            .collect::<HashSet<usize>>()
-            .len();
-        let fourLen = digits(4 * i)
-            .iter()
-            .cloned()
-            .collect::<HashSet<usize>>()
-            .len();
-        let fiveLen = digits(5 * i)
-            .iter()
-            .cloned()
-            .collect::<HashSet<usize>>()
-            .len();
-        let sixLen = digits(6 * i)
-            .iter()
-            .cloned()
-            .collect::<HashSet<usize>>()
-            .len();
-        
-        for &d in &firstLen {
-            if !twoLen.contains(&d) {
-                continue 'i;
-            }
+    let input = include_str!("input.txt");
+    let numbers = input.lines().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let mut acc = 0;
+    for (i, _) in numbers.iter().enumerate() {
+        if ( i >= 3 && numbers[i] > numbers[i - 3]) {
+            acc +=1;
         }
-        if firstLen.len() != twoLen.len() {
-            continue 'i;
-        }
-        println!("{}", i);
-        break;
     }
-    println!(
-        "{}",
-        std::time::Instant::now().duration_since(time).as_millis()
-    );
+    print!("{}",acc);
 }
