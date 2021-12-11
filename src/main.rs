2d706854de5e36ba27c50496da3255193ad45819
the 8th mage <eee34353@gmail.com>
2021-12-11 20:18:31 +0200

    advent 2021 3

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,83 @@ mod helpers;
 use helpers::*;
 fn main() {
     let input = include_str!("input.txt");
-    let mut depth = 0;
-    let mut horizontal = 0;
-    let mut aim = 0;
-    let numbers = input.lines().for_each(|s|
-        if (s.starts_with("forward ")) {
-            let x = s.trim_start_matches("forward ").parse::<i32>().unwrap();
-            horizontal += x;
-            depth += aim * x;
+    let nums: Vec<Vec<char>> = input.lines().map(|s| s.chars().collect()).collect();
+    let mut oxygen = nums.clone();
+    let mut co2 = nums.clone();
+    let mut newnum:i32 = 0;
+    let mut newnum_not:i32 = 0;
+    for i in 0..nums[0].len() {
+        let mut positivity = 0;
+        for n in 0..nums.len() {
+            if nums[n][i] == '0' {
+                positivity -= 1;
+            } else if nums[n][i] == '1' {
+                positivity += 1;
+            }
         }
-        else if (s.starts_with("up ")) {
-            aim -= s.trim_start_matches("up ").parse::<i32>().unwrap();
+        if positivity > 0 {
+            newnum += 1<<(nums[0].len() - i - 1);
+        } else if positivity < 0 {
+            newnum_not += 1<<(nums[0].len() - i - 1);
+        } else {
+            panic!();
         }
-        else if (s.starts_with("down ")) {
-            aim += s.trim_start_matches("down ").parse::<i32>().unwrap();
+    }
+    let n = oxygen[0].len();
+    for i in 0..n {
+        if (oxygen.len() == 1) {
+            break;
         }
+
+        let mut positivity = 0;
+        for n in 0..oxygen.len() {
+            if oxygen[n][i] == '0' {
+                positivity -= 1;
+            } else if oxygen[n][i] == '1' {
+                positivity += 1;
+            }
+        }
+        let bit = if positivity >= 0 {
+            '1'
+        } else {
+            '0'
+        }; 
+        oxygen = oxygen.into_iter().filter(|s| s[i] == bit).collect();
+    }
+
+    for i in 0..n {
+        if (co2.len() == 1) {
+            break;
+        }
+        let mut positivity = 0;
+        for n in 0..co2.len() {
+            if co2[n][i] == '0' {
+                positivity -= 1;
+            } else if co2[n][i] == '1' {
+                positivity += 1;
+            }
+        }
+        let bit = if positivity >= 0 {
+            '0'
+        } else {
+            '1'
+        }; 
+        co2 = co2.into_iter().filter(|s| s[i] == bit).collect();
+    }
+    let co2N = co2[0].iter().enumerate().fold(0, |acc, (index, &c)|{ if c == '1' {
+        acc + (1<<(n - index - 1))
+    } else {
+        acc
+    }
+}
     );
-    print!("{}", depth * horizontal);
+    let oxygenN = oxygen[0].iter().enumerate().fold(0, |acc, (index, &c)|{ if c == '1' {
+        acc + (1<<(n - index - 1))
+    } else {
+        acc
+    }
+}
+    );
+    println!("{}", newnum * newnum_not);
+    println!("{}", oxygenN * co2N);
 }
