bf23aa0b0d014f3dc5db63022f97a07931b425b6
the 8th mage <eee34353@gmail.com>
2021-12-11 21:02:43 +0200

    advent 2021 4

--- src/main.rs
+++ src/main.rs
@@ -4,83 +4,82 @@ mod helpers;
 use helpers::*;
 fn main() {
     let input = include_str!("input.txt");
-    let nums: Vec<Vec<char>> = input.lines().map(|s| s.chars().collect()).collect();
-    let mut oxygen = nums.clone();
-    let mut co2 = nums.clone();
-    let mut newnum:i32 = 0;
-    let mut newnum_not:i32 = 0;
-    for i in 0..nums[0].len() {
-        let mut positivity = 0;
-        for n in 0..nums.len() {
-            if nums[n][i] == '0' {
-                positivity -= 1;
-            } else if nums[n][i] == '1' {
-                positivity += 1;
+    let mut lines = input.lines();
+    let nums = lines
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse::<i32>().unwrap());
+    lines.next();
+    let mut boards = vec![];
+    let mut curr_board = vec![];
+    for line in lines {
+        if line == "" {
+            boards.push(curr_board);
+            curr_board = vec![];
+            continue;
+        }
+        let board_line: Vec<i32> = line
+            .split_ascii_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
+        curr_board.push(board_line);
+    }
+    boards.push(curr_board);
+    for popped_num in nums {
+        for board in boards.iter_mut() {
+            for line in board.iter_mut() {
+                for num in line.iter_mut() {
+                    if *num == popped_num {
+                        *num = -1;
+                    }
+                }
             }
         }
-        if positivity > 0 {
-            newnum += 1<<(nums[0].len() - i - 1);
-        } else if positivity < 0 {
-            newnum_not += 1<<(nums[0].len() - i - 1);
-        } else {
-            panic!();
-        }
-    }
-    let n = oxygen[0].len();
-    for i in 0..n {
-        if (oxygen.len() == 1) {
-            break;
-        }
-
-        let mut positivity = 0;
-        for n in 0..oxygen.len() {
-            if oxygen[n][i] == '0' {
-                positivity -= 1;
-            } else if oxygen[n][i] == '1' {
-                positivity += 1;
+        let mut board_index = 0;
+        loop {
+            if (board_index == boards.len()) {
+                break;
+            }
+            if (is_board_win(&boards[board_index])) {
+                let board = &boards[board_index];
+                let mut sum = 0;
+                for line in board {
+                    for num in line {
+                        if (*num != -1) {
+                            sum += *num;
+                        }
+                    }
+                }
+                boards.remove(board_index);
+                println!("score {}", sum * popped_num);
+            } else {
+                board_index +=1;
             }
         }
-        let bit = if positivity >= 0 {
-            '1'
-        } else {
-            '0'
-        }; 
-        oxygen = oxygen.into_iter().filter(|s| s[i] == bit).collect();
-    }
-
-    for i in 0..n {
-        if (co2.len() == 1) {
-            break;
-        }
-        let mut positivity = 0;
-        for n in 0..co2.len() {
-            if co2[n][i] == '0' {
-                positivity -= 1;
-            } else if co2[n][i] == '1' {
-                positivity += 1;
-            }
-        }
-        let bit = if positivity >= 0 {
-            '0'
-        } else {
-            '1'
-        }; 
-        co2 = co2.into_iter().filter(|s| s[i] == bit).collect();
-    }
-    let co2N = co2[0].iter().enumerate().fold(0, |acc, (index, &c)|{ if c == '1' {
-        acc + (1<<(n - index - 1))
-    } else {
-        acc
     }
 }
-    );
-    let oxygenN = oxygen[0].iter().enumerate().fold(0, |acc, (index, &c)|{ if c == '1' {
-        acc + (1<<(n - index - 1))
-    } else {
-        acc
+
+fn is_board_win(board: &Vec<Vec<i32>>) -> bool {
+    for i in 0..5 {
+        for j in 0..5 {
+            if (board[i][j] != -1) {
+                break;
+            }
+            if (j == 4) {
+                return true;
+            }
+        }
     }
-}
-    );
-    println!("{}", newnum * newnum_not);
-    println!("{}", oxygenN * co2N);
+    for i in 0..5 {
+        for j in 0..5 {
+            if (board[j][i] != -1) {
+                break;
+            }
+            if (j == 4) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
