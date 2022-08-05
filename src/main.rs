4d0cdeb9f7c90b51459115e29c8caf44b2b85cdb
the 8th mage <eee34353@gmail.com>
2022-08-05 16:37:20 +0300

    advent 2021 16

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,99 @@
+use core::num;
 // use core::panic;
 use std::{collections::*, thread::current, cmp::Reverse};
 // mod helpers;
 // use helpers::*;
 fn main() {
     let input = include_str!("input.txt");
-    let mut map: Vec<Vec<i32>> = input
-        .lines()
-        .map(|s| s.chars().map(|c| c as i32 - '0' as i32).collect())
+    let mut map: Vec<u8> = input
+        .chars()
+        .map(|s| u8::from_str_radix(&s.to_string(), 16).unwrap())
         .collect();
-    let mut new_map: Vec<Vec<i32>> = vec![vec![0; 5 * map.len()]; 5 * map.len()];
-    for i in 0..map.len() * 5 {
-        for j in 0..map.len() * 5 {
-            new_map[i][j] =
-                map[i % map.len()][j % map.len()] + (i / map.len() + j / map.len()) as i32;
-            if new_map[i][j] > 9 {
-                new_map[i][j] -= 9;
-            }
-        }
-    }
-    let mut hash_map = HashSet::new();
-    let mut vec = BinaryHeap::new();
-    vec.push(Reverse((0, 0, 0)));
-    let result = bfs(&(0, 0), &mut hash_map, &new_map, &mut vec);
-    println!("{}", result);
+    let num_bits = map.len() * 4;
+    let data = parse_packet(map.as_slice(), 0, num_bits);
+    println!("");
+    println!("{}", data.version_number);
+    println!("{}", data.version_sum);
+    println!("{}", data.len);
+    println!("{}", data.value);
 }
 
-fn bfs(
-    position: &(i32, i32),
-    hash_map: &mut HashSet<(i32, i32)>,
-    map: &Vec<Vec<i32>>,
-    free_queue: &mut BinaryHeap<Reverse<(i64, i32, i32)>>,
-) -> i64 {
-    while !free_queue.is_empty() {
-        let position = free_queue.pop().unwrap();
-        if !hash_map.get(&(position.0.1, position.0.2)).is_none() {
-            continue;
-        }
+struct ParsedPacket {
+    value: usize,
+    version_sum: usize,
+    version_number: usize,
+    len: usize
+}
 
-        if (position.0.1 as usize == map.len() - 1 && position.0.2 as usize == map.len() - 1) {
-            return position.0.0;
-        }
-        // println!("{} {} {}", position.0.0, position.0.1, position.0.2);
-        hash_map.insert((position.0.1, position.0.2));
-        if position.0.1 < map.len() as i32 - 1 {
-            if hash_map.get(&(position.0.1 + 1, position.0.2)).is_none() {
-                free_queue.push(Reverse((
-                    position.0.0 + map[position.0.1 as usize + 1][position.0.2 as usize] as i64,
-                    position.0.1 + 1,
-                    position.0.2,
-                )));
+fn parse_packet(map: &[u8], starting_bit: usize, end_bit: usize) -> ParsedPacket{
+    let mut version = get_number(map, starting_bit + 0..starting_bit + 3);
+    let mut version_sum = version;
+    let packet_type = get_number(map, starting_bit + 3..starting_bit + 6);
+    let mut current_bit = starting_bit + 6;
+    let mut value = 0;
+    if packet_type == 4 {
+        loop  {
+            value = value * 16 + get_number(map, current_bit + 1.. current_bit + 5);            
+            if get_bit(map, current_bit) == 0 {
+                current_bit += 5;
+                break;
             }
+            current_bit += 5;
         }
-        if position.0.2 < map.len() as i32 - 1 {
-            if hash_map.get(&(position.0.1, position.0.2 + 1)).is_none() {
-                free_queue.push(Reverse((
-                    position.0.0 + map[position.0.1 as usize][position.0.2 as usize + 1] as i64,
-                    position.0.1,
-                    position.0.2 + 1,
-                )));
+    } else {
+        value = match packet_type {
+            0 => 0,
+            1 => 1,
+            2 => usize::max_value(),
+            3 => usize::min_value(),
+            5 => 0,
+            6 => 0,
+            7 => 0,
+            _ => panic!(), 
+        };
+        let length_type = get_bit(map, starting_bit + 6);
+        if length_type == 0 {
+            let len = get_number(map, starting_bit + 7..starting_bit + 7 + 15);
+            current_bit = starting_bit + 7 + 15;
+            let mut index = 0;
+            while current_bit - starting_bit - 7 - 15 != len {
+                let parsed_packet = parse_packet(&map, current_bit, len + starting_bit);
+                current_bit = parsed_packet.len + current_bit;
+                value = update_value(packet_type, value, parsed_packet.value, index);
+                version_sum += parsed_packet.version_sum;
+                index += 1;
             }
-        }
-        if position.0.1 > 0 {
-            if hash_map.get(&(position.0.1 - 1, position.0.2)).is_none() {
-                free_queue.push(Reverse((
-                    position.0.0 + map[position.0.1 as usize - 1][position.0.2 as usize] as i64,
-                    position.0.1 - 1,
-                    position.0.2,
-                )));
-            }
-        }
-        if position.0.2 > 0 {
-            if hash_map.get(&(position.0.1, position.0.2 - 1)).is_none() {
-                free_queue.push(Reverse((
-                    position.0.0 + map[position.0.1 as usize][position.0.2 as usize - 1] as i64,
-                    position.0.1,
-                    position.0.2 - 1,
-                )));
+        } else {
+            let len = get_number(map, starting_bit + 7..starting_bit +7 + 11);
+            current_bit = starting_bit + 7 + 11;
+            for i in 0.. len {
+                let parsed_packet = parse_packet(&map, current_bit, end_bit);
+                current_bit = parsed_packet.len + current_bit;
+                value = update_value(packet_type, value, parsed_packet.value, i);
+                version_sum += parsed_packet.version_sum;
             }
         }
     }
-    return 0;
+    ParsedPacket {value, version_sum, version_number: version, len: current_bit - starting_bit }
+}
+
+fn get_bit(map: &[u8], bit: usize) -> u8 {
+    (map[bit / 4] >> (3-(bit%4))) % 2
+}
+
+fn get_number(map: &[u8], bits: impl std::iter::Iterator<Item = usize>) -> usize {
+    bits.fold(0,|acc, bit| acc * 2 + get_bit(map, bit) as usize)
+}
+
+fn update_value(packet_type: usize, value: usize, new_value: usize, index: usize) -> usize {
+    match packet_type {
+        0=> return value + new_value,
+        1=> return value * new_value,
+        2=> return value.min(new_value),
+        3=> return value.max(new_value),
+        5=> if index == 0 { return new_value} else {return (value > new_value) as usize}
+        6=> if index == 0 { return new_value} else {return (value < new_value) as usize}
+        7=> if index == 0 { return new_value} else {return (value == new_value) as usize}
+        _ => panic!()
+    }
 }
