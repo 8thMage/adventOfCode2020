5be4886bd72c05edfde11ce37525ab3ebcd033d2
the 8th mage <eee34353@gmail.com>
2022-08-12 12:15:48 +0300

    advent 24

--- src/main.rs
+++ src/main.rs
@@ -1,231 +1,254 @@
-use std::{collections::HashMap, default, hash::Hash, iter::repeat};
-
-#[derive(Clone, PartialEq, Hash, Eq)]
-struct Positions {
-    pos: [Vec<(i8, i32, i32)>; 4],
-}
+use std::{collections::{HashMap, HashSet}, thread::current};
 
 fn main() {
     let input = include_str!("input.txt");
-    let mut pos = HashMap::new();
-    let mut a_count = 0;
-    let mut b_count = 0;
-    let mut c_count = 0;
-    let mut d_count = 0;
+    let mut parameters0 = vec![];
+    let mut parameters1 = vec![];
+    let mut parameters2 = vec![];
     for (line_number, line) in input.lines().enumerate() {
-        if line_number == 1 || line_number == 2 || line_number == 3 || line_number == 4 || line_number == 5 {
-            for (char_number, char) in line.chars().enumerate() {
-                if char == 'A' {
-                    pos.insert(
-                        ('A', a_count),
-                        (0, char_number as i32 - 1, line_number as i32 - 1),
-                    );
-                    a_count += 1;
-                }
+        let other_line = input.lines().nth(line_number % 18).unwrap();
+        if line_number % 18 == 4 || line_number % 18 == 5 || line_number % 18 == 15 {
+            assert_eq!(
+                line.split_once(" ").unwrap().0,
+                other_line.split_once(" ").unwrap().0
+            );
 
-                if char == 'B' {
-                    pos.insert(
-                        ('B', b_count),
-                        (0, char_number as i32 - 1, line_number as i32 - 1),
-                    );
-                    b_count += 1;
-                }
-
-                if char == 'C' {
-                    pos.insert(
-                        ('C', c_count),
-                        (0, char_number as i32 - 1, line_number as i32 - 1),
-                    );
-                    c_count += 1;
-                }
-
-                if char == 'D' {
-                    pos.insert(
-                        ('D', d_count),
-                        (0, char_number as i32 - 1, line_number as i32 - 1),
-                    );
-                    d_count += 1;
-                }
+            assert_eq!(
+                line.split_once(" ").unwrap().1.split_once(" ").unwrap().0,
+                other_line
+                    .split_once(" ")
+                    .unwrap()
+                    .1
+                    .split_once(" ")
+                    .unwrap()
+                    .0
+            );
+            if line_number % 18 == 4 {
+                parameters2.push(
+                    i64::from_str_radix(
+                        line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        10,
+                    )
+                    .unwrap(),
+                )
+            } else if line_number % 18 == 5 {
+                parameters0.push(
+                    i64::from_str_radix(
+                        line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        10,
+                    )
+                    .unwrap(),
+                )
+            } else {
+                parameters1.push(
+                    i64::from_str_radix(
+                        line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        10,
+                    )
+                    .unwrap(),
+                )
             }
+        } else if line_number % 18 == 0 {
+            assert_eq!(line, "inp w");
+        } else {
+            assert_eq!(line, other_line);
         }
     }
-    let x =0;
-    // let map = Positions {
-    //     pos: [
-    //         vec![*pos.get(&('A', 0)).unwrap(), *pos.get(&('A', 1)).unwrap()],
-    //         vec![*pos.get(&('B', 0)).unwrap(), *pos.get(&('B', 1)).unwrap()],
-    //         vec![*pos.get(&('C', 0)).unwrap(), *pos.get(&('C', 1)).unwrap()],
-    //         vec![*pos.get(&('D', 0)).unwrap(), *pos.get(&('D', 1)).unwrap()],
-    //     ],
-    // };
-    // println!(
-    //     "{}",
-    //     find_min_energy(&map, 0, &mut HashMap::new(), 2).unwrap()
-    // );
-
-    // for pos in pos.iter_mut() {
-    //     if pos.1 .2 == 2 {
-    //         pos.1 .2 = 4
-    //     }
-    // }
-    // let map2 = Positions {
-    //     pos: [
-    //         vec![
-    //             *pos.get(&('A', 0)).unwrap(),
-    //             *pos.get(&('A', 1)).unwrap(),
-    //             (0, 8, 2),
-    //             (0, 6, 3),
-    //         ],
-    //         vec![
-    //             *pos.get(&('B', 0)).unwrap(),
-    //             *pos.get(&('B', 1)).unwrap(),
-    //             (0, 6, 2),
-    //             (0, 4, 3),
-    //         ],
-    //         vec![
-    //             *pos.get(&('C', 0)).unwrap(),
-    //             *pos.get(&('C', 1)).unwrap(),
-    //             (0, 4, 2),
-    //             (0, 8, 3),
-    //         ],
-    //         vec![
-    //             *pos.get(&('D', 0)).unwrap(),
-    //             *pos.get(&('D', 1)).unwrap(),
-    //             (0, 2, 2),
-    //             (0, 2, 3),
-    //         ],
-    //     ],
-    // };
-
-    let map2 = Positions {
-        pos: [
-            vec![
-                *pos.get(&('A', 0)).unwrap(),
-                *pos.get(&('A', 1)).unwrap(),
-                *pos.get(&('A', 2)).unwrap(),
-                *pos.get(&('A', 3)).unwrap(),
-            ],
-            vec![
-                *pos.get(&('B', 0)).unwrap(),
-                *pos.get(&('B', 1)).unwrap(),
-                *pos.get(&('B', 2)).unwrap(),
-                *pos.get(&('B', 3)).unwrap(),
-            ],
-            vec![
-                *pos.get(&('C', 0)).unwrap(),
-                *pos.get(&('C', 1)).unwrap(),
-                *pos.get(&('C', 2)).unwrap(),
-                *pos.get(&('C', 3)).unwrap(),
-            ],
-            vec![
-                *pos.get(&('D', 0)).unwrap(),
-                *pos.get(&('D', 1)).unwrap(),
-                *pos.get(&('D', 2)).unwrap(),
-                *pos.get(&('D', 3)).unwrap(),
-            ],
-        ],
-    };
-
+    let max = u64::max_value();
+    let mut set = HashSet::new();
     println!(
         "{}",
-        find_min_energy(&map2, 0, &mut HashMap::new(), 4).unwrap()
+        tree(
+            &[
+                parameters0.clone(),
+                parameters1.clone(),
+                parameters2.clone()
+            ],
+            0,
+            0,
+            0,
+            &mut set,
+        )
+        .unwrap()
     );
+    println!("run");
+    for input_value in (11111111111111..100000000000000i64).rev() {
+        let mut reg_w = 0;
+        let mut reg_z = 0;
+        let mut input_char = 0;
+        let mut div = 10000000000000i64;
+        for i in 0..14 {
+            reg_w = (input_value / div) % 10;
+            div /= 10;
+            reg_z = if reg_z % 26 + parameters0[i] != reg_w {
+                (reg_z / parameters2[i]) * 26 + reg_w + parameters1[i]
+            } else {
+                reg_z / parameters2[i]
+            }
+        }
+        if reg_z == 0 {
+            let x = 0;
+        }
+    }
+
+    // let mut map = HashMap::new();
+    let mut input_lines = vec![];
+    for input_value in (11111111111111..100000000000000i64).rev() {
+        let mut regs = [0; 4];
+        let input_value = input_value.to_string();
+        let mut input_char = 0;
+        let mut known_values = 0;
+        let mut skip_number = 0;
+        // while map.contains_key(&input_value[0..known_values + 1]) {
+        //     skip_number = input_lines[known_values + 1] + 1;
+        //     input_char += 1;
+        //     regs = map[&input_value[0..known_values + 1]];
+        //     known_values += 1;
+        // }
+        for (line_number, line) in input.lines().enumerate().skip(skip_number) {
+            match line.split_once(" ").unwrap().0 {
+                "inp" => {
+                    let register = get_register(line.split_once(" ").unwrap().1);
+                    regs[register] = i64::from_str_radix(
+                        &input_value.chars().nth(input_char).unwrap().to_string(),
+                        10,
+                    )
+                    .unwrap();
+                    input_char += 1;
+                    // if !map.contains_key(&input_value[0..input_char]) {
+                    //     map.insert(input_value[0..input_char].to_owned(), regs);
+                    // }
+                    if input_lines.len() < 14 {
+                        input_lines.push(line_number);
+                    }
+                }
+
+                "add" => {
+                    let register =
+                        get_register(line.split_once(" ").unwrap().1.split_once(" ").unwrap().0);
+                    let second_str = line.split_once(" ").unwrap().1.split_once(" ").unwrap().1;
+                    let value = i64::from_str_radix(&second_str, 10).unwrap_or_else(|_| {
+                        let second_reg = get_register(
+                            line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        );
+                        regs[second_reg]
+                    });
+                    regs[register] = regs[register] + value;
+                }
+                "mul" => {
+                    let register =
+                        get_register(line.split_once(" ").unwrap().1.split_once(" ").unwrap().0);
+                    let second_str = line.split_once(" ").unwrap().1.split_once(" ").unwrap().1;
+                    let value = i64::from_str_radix(&second_str, 10).unwrap_or_else(|_| {
+                        let second_reg = get_register(
+                            line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        );
+                        regs[second_reg]
+                    });
+                    regs[register] = regs[register] * value;
+                }
+                "div" => {
+                    let register =
+                        get_register(line.split_once(" ").unwrap().1.split_once(" ").unwrap().0);
+                    let second_str = line.split_once(" ").unwrap().1.split_once(" ").unwrap().1;
+                    let value = i64::from_str_radix(&second_str, 10).unwrap_or_else(|_| {
+                        let second_reg = get_register(
+                            line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        );
+                        regs[second_reg]
+                    });
+                    regs[register] = regs[register] / value;
+                }
+
+                "mod" => {
+                    let register =
+                        get_register(line.split_once(" ").unwrap().1.split_once(" ").unwrap().0);
+                    let second_str = line.split_once(" ").unwrap().1.split_once(" ").unwrap().1;
+                    let value = i64::from_str_radix(&second_str, 10).unwrap_or_else(|_| {
+                        let second_reg = get_register(
+                            line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        );
+                        regs[second_reg]
+                    });
+                    regs[register] = regs[register] % value;
+                }
+                "eql" => {
+                    let register =
+                        get_register(line.split_once(" ").unwrap().1.split_once(" ").unwrap().0);
+                    let second_str = line.split_once(" ").unwrap().1.split_once(" ").unwrap().1;
+                    let value = i64::from_str_radix(&second_str, 10).unwrap_or_else(|_| {
+                        let second_reg = get_register(
+                            line.split_once(" ").unwrap().1.split_once(" ").unwrap().1,
+                        );
+                        regs[second_reg]
+                    });
+                    regs[register] = (regs[register] == value) as i64;
+                }
+
+                _ => {
+                    panic!()
+                }
+            }
+        }
+        if regs[3] == 0 {
+            println!("{}", input_value);
+            break;
+        }
+    }
 }
 
-fn find_min_energy(
-    map: &Positions,
-    current_score: i64,
-    hash: &mut HashMap<Positions, Option<i64>>,
-    last_y: i32,
+fn get_register(str: &str) -> usize {
+    match str {
+        "w" => 0,
+        "x" => 1,
+        "y" => 2,
+        "z" => 3,
+        _ => panic!(),
+    }
+}
+
+fn tree(
+    parameters: &[Vec<i64>; 3],
+    index: usize,
+    current_z: i64,
+    current_value: i64,
+    map: &mut HashSet<(i64, usize)>,
 ) -> Option<i64> {
-    if hash.contains_key(&map) {
-        return hash.get(&map).unwrap().map(|m| m + current_score);
+    if map.contains(&(current_z, index)) {
+        return None;
     }
-    'letter: for letter in 0..map.pos.len() {
-        for number in 0..map.pos[letter].len() {
-            let (moves, current_x, current_y) = map.pos[letter][number];
-            if current_x != 2 + letter as i32 * 2 || current_y == 0 {
-                break 'letter;
-            }
+    if index == 14 {
+        // print_number(current_z);
+        // println!("");
+        if current_z == 0 {
+            return Some(current_value);
         }
-        if letter == map.pos.len() - 1 {
-            return Some(current_score);
-        }
+        map.insert((current_z, index));
+        return None;
     }
-    let mut minimum = None;
-    for letter in 0..map.pos.len() {
-        if map.pos[letter].iter().all(|pos| pos.1 == letter as i32 * 2 + 2)          
-        {
-            continue;
-        }
-        'next_number: for number in 0..map.pos[letter].len() {
-            if current_score == 0 || current_score == 3000 {
-                let z = 0;
-            }
-            let mut new_map = map.clone();
-            if map.pos[letter][number].2 != 0 {
-                let (moves, current_x, current_y) = map.pos[letter][number];
-                if moves == 2 {
-                    continue;
-                }
-                'next_x: for x in 0..11 {
-                    if x == 2 || x == 4 || x == 6 || x == 8 {
-                        continue;
-                    }
-                    for &(_, other_x, other_y) in map.pos.iter().map(|x| x.iter()).flatten() {
-                        if other_y == 0
-                            && (other_x == x
-                                || ((other_x <= x && other_x > current_x)
-                                    || (other_x >= x && other_x < current_x)))
-                        {
-                            continue 'next_x;
-                        } else if other_x == current_x && current_y > other_y {
-                            continue 'next_number;
-                        }
-                    }
-                    new_map.pos[letter][number] = (moves + 1, x, 0);
-                    let new_score = current_score
-                        + (((x - current_x).abs() + current_y) as i64) * 10i64.pow(letter as u32);
-                    if let Some(score) = find_min_energy(&new_map, new_score, hash, last_y) {
-                        minimum = Some(minimum.unwrap_or(i64::max_value()).min(score));
-                    }
-                }
-            } else {
-                let (moves, current_x, current_y) = map.pos[letter][number];
-                if moves == 2 {
-                    continue;
-                }
-                let mut next_y = last_y;
-                let next_x = letter as i32 * 2 + 2;
-                for (other_letter, &(moves, other_x, other_y)) in map
-                    .pos
-                    .iter()
-                    .enumerate()
-                    .map(|x| repeat(x.0).zip(x.1.iter()))
-                    .flatten()
-                {
-                    if other_y == 0
-                        && ((other_x < next_x && other_x > current_x)
-                            || (other_x > next_x && other_x < current_x))
-                    {
-                        continue 'next_number;
-                    } else if other_x == next_x {
-                        if other_letter == letter {
-                            next_y = next_y.min(other_y - 1);
-                        } else {
-                            continue 'next_number;
-                        }
-                    }
-                }
-                new_map.pos[letter][number] = (2, next_x, next_y);
-                let new_score = current_score
-                    + (((next_x - current_x).abs() + next_y) as i64) * 10i64.pow(letter as u32);
-                if let Some(score) = find_min_energy(&new_map, new_score, hash, last_y) {
-                    minimum = Some(minimum.unwrap_or(i64::max_value()).min(score));
-                }
-            }
+    for i in (1i64..10) {
+        let reg_z = if current_z % 26 + parameters[0][index] != i {
+            (current_z / parameters[2][index]) * 26 + i + parameters[1][index]
+        } else {
+            current_z / parameters[2][index]
+        };
+
+        let new_current_value = 10 * current_value + i;
+        if let Some(value) = tree(parameters, index + 1, reg_z, new_current_value, map) {
+            return Some(value);
         }
     }
-    hash.insert(map.clone(), minimum.map(|m| m - current_score));
-    return minimum;
+    map.insert((current_z, index));
+    return None;
+}
+
+fn print_number(s: i64) {
+    if s == 0 {
+        return;
+    }
+    print_number(s/26);
+    print!(
+        "{}",
+        String::from_utf8(vec![('a' as u8 + (s % 26) as u8)]).unwrap()
+    );
 }
