3374d0eb96b87c150428ab5bad578aa147fe811f
the 8th mage <eee34353@gmail.com>
2022-10-04 22:51:01 +0300

    euler 801

--- src/main.rs
+++ src/main.rs
@@ -4,76 +4,50 @@ use std::{
 };
 
 fn main() {
-    let input = include_str!("input.txt");
-    let mut map = vec![];
-    for (_line_number, line) in input.lines().enumerate() {
-        map.push(vec![]);
-        for (_char_number, char) in line.chars().enumerate() {
-            if char == '.' {
-                map.last_mut().unwrap().push(0);
-            } else if char == '>' {
-                map.last_mut().unwrap().push(1);
-            } else if char == 'v' {
-                map.last_mut().unwrap().push(2);
+    let time = std::time::Instant::now();
+    let seive_size = 1 << 28;
+    let mut seive = vec![false; seive_size];
+    seive[0] = true;
+    seive[1] = true;
+    let mut count = 0;
+    for i in 2..seive_size {
+        if seive[i] == false {
+            count += 1;
+            // println!("{:b}", i);
+            if count == 5_000_000 {
+                let duration = time.elapsed().as_millis();
+                println!("5_000_000th prime {}", i);
+                println!("time {}", duration);
+            }
+            let mut j = 2;
+            loop {
+                let index = carry_less_mul(i as u64, j);
+                if index as usize >= seive.len() * 2 {
+                    break;
+                }
+                j += 1;
+                if index as usize >= seive.len() {
+                    continue;
+                }
+                seive[index as usize] = true;
             }
         }
     }
-    let mut counter = 0;
-    loop {
-        let map2 = map_move_left(&map);
-        let map3 = map_move_down(&map2);
-        if map3
-            .iter()
-            .flat_map(|v| v.iter())
-            .zip(map.iter().flat_map(|v| v.iter()))
-            .all(|(e, e2)| e == e2)
-        {
-            println!("{}", counter + 1);
-            break;
-        }
-         counter += 1;
-        map = map3;
-    }
+    println!("count {}", count);
+    let x = 0;
 }
 
-fn map_move_left(map: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut new_map = vec![vec![0; map[0].len()]; map.len()];
-    for (y, v) in map.iter().enumerate() {
-        for (x, e) in v.iter().enumerate() {
-            if new_map[y][x] != 0 {
-                continue;
-            }
-            if *e != 1 {
-                new_map[y][x] = map[y][x];
-            } else {
-                if map[y][(x + 1) % map[0].len()] == 0 {
-                    new_map[y][(x + 1) % map[0].len()] = 1;
-                } else {
-                    new_map[y][x] = 1;
-                }
-            }
-        }
+fn carry_less_mul(mut a: u64, mut b: u64) -> u64 {
+    if a < b {
+        return carry_less_mul(b, a);
     }
-    new_map
-}
-
-fn map_move_down(map: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut new_map = vec![vec![0; map[0].len()]; map.len()];
-    for (y, v) in map.iter().enumerate() {
-        for (x, e) in v.iter().enumerate() {
-            if new_map[y][x] != 0 {
-                continue;
-            }
-            if *e != 2 {
-                new_map[y][x] = map[y][x];
-            } else {
-                if map[(y + 1) % map.len()][x] == 0 {
-                    new_map[(y + 1) % map.len()][x] = 2;
-                } else {
-                    new_map[y][x] = 2;
-                }
-            }
+    let mut sum = 0;
+    while b != 0 {
+        if b % 2 != 0 {
+            sum ^= a;
         }
+        a <<= 1;
+        b >>= 1;
     }
-    new_map
+    sum
 }
