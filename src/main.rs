38c5671624e4e3e02c9e0873765cebfd7017b1c9
the 8th mage <eee34353@gmail.com>
2023-01-06 23:02:23 +0200

    advent 3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,63 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("input.txt");
     let mut sum = 0;
-    for line in input.lines() {
-        let strs = line.split_once(" ").unwrap_or(("0","0"));
-        let op = strs.0.chars().next().unwrap() as usize - 'A' as usize;
-        let you = strs.1.chars().next().unwrap() as usize - 'X' as usize;
-        let you2 = (2+ op + you)% 3;
-        sum += you2+1;
-        // println!("  {}",sum);
+    // for line in input.lines() {
+    //     let split_line = line.split_at(line.len() / 2);
+    //     println!("{}", split_line.0);
+    //     let mut hash_1 = HashSet::new();
+    //     let mut hash_2 = HashSet::new();
+    //     for char in split_line.0.chars() {
+    //         hash_1.insert(char);
+    //     }
+    //     for char in split_line.1.chars() {
+    //         hash_2.insert(char);
+    //     }
+    //     let share = hash_1.intersection(&hash_2).next().unwrap();
+    //     println!("{}", share);
+    //     if share.is_uppercase() {
+    //         sum += 26;
+    //     }
+    //     sum += share.to_lowercase().next().unwrap() as usize - 'a' as usize + 1;
+    // }
+    let mut lineA = None;
+    let mut lineB = None;
 
-        sum += match (6+you2 - op) % 3 {
-            0 => 3 ,
-            1 => 6,
-            2 => 0,
-            _=>0,
-        };
-        // println!("{}",sum);
+    for line in input.lines() {
+        if lineA.is_none() {
+            lineA = Some(line);
+            continue;
+        }
+        if lineB.is_none() {
+            lineB = Some(line);
+            continue;
+        }
+        // let split_line = line.split_at(line.len() / 2);
+        let mut hash_1 = HashSet::new();
+        let mut hash_2 = HashSet::new();
+        let mut hash_3 = HashSet::new();
+        for char in lineA.unwrap().chars() {
+            hash_1.insert(char);
+        }
+        for char in lineB.unwrap().chars() {
+            hash_2.insert(char);
+        }
+        for char in line.chars() {
+            hash_3.insert(char);
+        }
+        let hash_12 = hash_1
+            .intersection(&hash_2)
+            .map(|c| *c)
+            .collect::<HashSet<char>>();
+        let share = hash_12.intersection(&hash_3).next().unwrap();
+        println!("{}", share);
+        if share.is_uppercase() {
+            sum += 26;
+        }
+        sum += share.to_lowercase().next().unwrap() as usize - 'a' as usize + 1;
+        lineA = None;
+        lineB = None;
     }
-    println!("{}",sum);
+    println!("{}", sum);
 }
