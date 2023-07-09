56d354704b309c3154e9fceab3ca581248282679
Eden Segal <Eee34353@gmail.com>
2023-07-09 22:14:45 +0300

    advent 18

--- src/main.rs
+++ src/main.rs
@@ -9,95 +9,89 @@ use std::{
 
 fn main() {
     let input = include_str!("input.txt");
-    let mut dirs = input.chars().map(|c| if c == '<' { -1 } else { 1 }).cycle();
-    let rocks_str = include_str!("rocks.rs");
-    let iter = rocks_str.split("\n\n");
-    let mut rocks = vec![];
-    for rock_str in iter {
-        let mut rock = vec![];
-        for line in rock_str.lines() {
-            rock.push(
-                line.chars()
-                    .map(|c| if c == '#' { 1 } else { 0 })
-                    .collect::<Vec<_>>(),
-            )
-        }
-        rock.reverse();
-        rocks.push(rock);
+    let mut stones = HashSet::new();
+    let mut max_in_any_dim = 0;
+    for line in input.lines() {
+        let vec = line
+            .split(",")
+            .map(|s| i32::from_str(s).unwrap())
+            .collect::<Vec<_>>();
+        max_in_any_dim = max_in_any_dim.max(*vec.iter().max().unwrap());
+        stones.insert(vec);
     }
-    let mut map = vec![vec![true; 9]];
-    let mut counter = -1;
-    // let mut delta_height = vec![];
-    for rock in rocks.iter().cycle().take(1002220) {
-        counter += 1;
-        let mut y = map.len() + 3;
-        let mut x = 3;
-        loop {
-            let dir = dirs.next().unwrap();
-            let new_x = (x as i32 + dir) as usize;
-            if !get_rock_iter(rock, new_x, y).any(|(ry, rx)| {
-                rx == 8
-                    || rx == 0
-                    || (map.get(ry).is_some()
-                        && map.get(ry).unwrap().get(rx).is_some()
-                        && *map.get(ry).unwrap().get(rx).unwrap())
-            }) {
-                x = new_x;
-            }
-            let should_stop = get_rock_iter(rock, x, y).any(|(ry, rx)| {
-                map.get(ry - 1).is_some()
-                    && map.get(ry - 1).unwrap().get(rx).is_some()
-                    && *map.get(ry - 1).unwrap().get(rx).unwrap()
-            });
-            if should_stop {
-                let prev_len = map.len();
-                for (ry, rx) in get_rock_iter(rock, x, y) {
-                    if map.get(ry).is_none() {
-                        map.push(vec![false; 9]);
-                        map[ry][0] = true;
-                        map[ry][8] = true;
-                    }
-                    map[ry][rx] = true;
+    let mut counter = 0;
+    let mut counter2 = 0;
+    let mut interior = HashSet::new();
+    let mut exterior = HashSet::new();
+    for entry in stones.iter() {
+        for i in 0..entry.len() {
+            for d in [-1, 1] {
+                let mut new_entry = entry.clone();
+                new_entry[i] += d;
+                if !stones.contains(&new_entry) {
+                    counter += 1;
+                    counter2 += fill(
+                        &new_entry,
+                        &stones,
+                        &mut interior,
+                        &mut exterior,
+                        max_in_any_dim,
+                    );
                 }
-                if counter >= 10000 {
-                    // delta_height.push(map.len() - prev_len);
-                    // println!("{} {}", counter, map.len() - prev_len);
-                    if counter % 10000 == 0 {
-                        println!("{} {}",counter,  map.len() - 1);
-                    }
-                }
-
-                break;
             }
-            y = y - 1;
         }
     }
-    // for y in 0..1000 {
-    //     print!("{}\t", y);
-    //     for x in 0..map[y].len() {
-    //         print!("{}", if map[y][x] { "#" } else { "." })
-    //     }
-    //     print!("\t");
-    //     for x in 0..map[y].len() {
-    //         print!("{}", if map[y][x] == map[y + 400][x] { "#" } else { "." })
-    //     }
-    //     println!("");
-    // }
-    println!("{}", map.len() - 1);
+    println!("{}", counter);
+    println!("{}", counter2);
 }
 
-fn get_rock_iter(
-    rock: &Vec<Vec<i8>>,
-    x: usize,
-    y: usize,
-) -> impl Iterator<Item = (usize, usize)> + '_ {
-    rock.iter().enumerate().flat_map(move |(ry, arr)| {
-        iter::repeat(y + ry).zip(arr.iter().enumerate().filter_map(move |(rx, v)| {
-            if *v == 1 {
-                Some(x + rx)
-            } else {
-                None
+fn fill(
+    input: &Vec<i32>,
+    stones: &HashSet<Vec<i32>>,
+    interior: &mut HashSet<Vec<i32>>,
+    exterior: &mut HashSet<Vec<i32>>,
+    max_in_any_dim: i32,
+) -> i32 {
+    let mut should_be_filled = Vec::new();
+    let mut filled = HashSet::new();
+    should_be_filled.push(input.clone());
+    while let Some(entry) = should_be_filled.pop() {
+        for i in 0..entry.len() {
+            for d in [-1, 1] {
+                let mut new_entry = entry.clone();
+                new_entry[i] += d;
+                if stones.contains(&new_entry) {
+                    continue;
+                }
+                if interior.contains(&new_entry) {
+                    for i in filled.into_iter() {
+                        interior.insert(i);
+                    }
+                    while let Some(x) = should_be_filled.pop() {
+                        interior.insert(x);
+                    }
+                    return 0;
+                }
+                if exterior.contains(&new_entry)
+                    || new_entry.iter().any(|&s| s < 0 || s > max_in_any_dim)
+                {
+                    for i in filled.into_iter() {
+                        exterior.insert(i);
+                    }
+                    while let Some(x) = should_be_filled.pop() {
+                        exterior.insert(x);
+                    }
+                    return 1;
+                }
+                if !should_be_filled.contains(&new_entry) && !filled.contains(&new_entry) {
+                    should_be_filled.push(new_entry);
+                }
             }
-        }))
-    })
+        }
+        filled.insert(entry);
+    }
+    for i in filled.into_iter() {
+        interior.insert(i);
+    }
+    return 0;
 }
