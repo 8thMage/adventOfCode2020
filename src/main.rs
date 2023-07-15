65310e113ace3baf0340a396cbdd9284e525c110
Eden Segal <Eee34353@gmail.com>
2023-07-15 19:11:35 +0300

    advent day 20

--- src/main.rs
+++ src/main.rs
@@ -10,143 +10,33 @@ use std::{
 
 fn main() {
     let input = include_str!("input.txt");
-    let mut sum = 1;
-    for line in input.lines().take(3) {
-        let mut iter = line
-            .split(['.', ':'])
-            .map(|s| {
-                s.split(" ")
-                    .filter_map(|s| u32::from_str(s).ok())
-                    .collect::<Vec<_>>()
-            })
-            .filter(|v| v.len() > 0);
-        let index = iter.next().unwrap()[0];
-        let mut resources_per_robot = iter.collect::<Vec<_>>();
-        for res in &mut resources_per_robot {
-            if res.len() == 1 {
-                res.push(0);
+    let array = input
+        .lines()
+        .map(|s| i64::from_str(s).unwrap() * 811589153)
+        .collect::<Vec<_>>();
+    // let array = vec![1, 2, -3, 0, 3, 7, -2];
+    let mut current_array = array.iter().enumerate().collect::<Vec<_>>();
+    let len = array.len() - 1;
+    let arr_len = array.len();
+
+    for _ in 0..10 {
+        for i in 0..array.len() {
+            // let element = 7;
+            let position = current_array.iter().position(|&e| e.0 == i).unwrap();
+            let mut new_position =
+                ((current_array[position].1 % len as i64) + position as i64) % len as i64;
+            if new_position < 0 {
+                new_position += len as i64;
             }
+            let element = current_array[position];
+            current_array.remove(position);
+            current_array.insert(new_position as usize, element);
         }
-        resources_per_robot[3].insert(1, 0);
-        for res in &mut resources_per_robot {
-            if res.len() == 2 {
-                res.push(0);
-            }
-        }
-        sum *= 
-             create_robots(
-                &resources_per_robot,
-                [1, 0, 0, 0],
-                [0; 4],
-                false,
-                0,
-                &mut HashMap::new(),
-            );
     }
-    println!("{}", sum);
-}
-
-#[derive(PartialEq, Eq, PartialOrd)]
-struct Entry {
-    robots: [u32; 4],
-    resources: [u32; 4],
-    minute: u32,
-    previously_built: bool,
-}
-
-impl Ord for Entry {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        ((self.resources[3] + self.robots[3] * self.minute) * 1000
-            + (self.resources[2] + self.robots[2] * self.minute))
-            .cmp(
-                &((other.resources[3] + other.robots[3] * other.minute) * 1000
-                    + (other.resources[2] + other.robots[2] * other.minute)),
-            )
-    }
-}
-
-fn create_robots(
-    resources_per_robots: &Vec<Vec<u32>>,
-    mut robots: [u32; 4],
-    mut resources: [u32; 4],
-    previously_built: bool,
-    minute: u32,
-    state_hash: &mut HashMap<Entry, u32>,
-) -> u32 {
-    let mut heap = std::collections::BinaryHeap::new();
-    // robots = [1, 0, 0, 0];
-    // resources = [4, 15, 0, 0];
-    // let minute = 10;
-    heap.push(Entry {
-        robots,
-        resources,
-        previously_built,
-        minute,
-    });
-    let mut max = 0;
-    while !heap.is_empty() {
-        let Entry {
-            mut robots,
-            mut resources,
-            previously_built,
-            minute,
-        } = heap.pop().unwrap();
-        if minute == 32 {
-            max = max.max(*resources.last().unwrap());
-            continue;
-        }
-        let delta_time = 32 - minute;
-        if resources[3] + robots[3] * delta_time + (delta_time) * (delta_time - 1) / 2 <= max {
-            continue;
-        }
-
-        let possible_robots = resources_per_robots
-            .iter()
-            .enumerate()
-            .filter(|(_, v)| {
-                v[0] <= resources[0]
-                    && v[1] <= resources[1]
-                    && v[2] <= resources[2]
-                    && (previously_built
-                        || (v[0] > (resources[0] - robots[0]) || v[1] > (resources[1] - robots[1]) || v[2] > (resources[2] - robots[2])))
-            })
-            .map(|(i, _v)| i)
-            .collect::<Vec<_>>();
-
-        for i in 0..resources.len() {
-            resources[i] += robots[i];
-        }
-        for &robot in possible_robots.iter().rev() {
-            resources[0] -= resources_per_robots[robot][0];
-            resources[1] -= resources_per_robots[robot][1];
-            resources[2] -= resources_per_robots[robot][2];
-            robots[robot] += 1;
-            let delta_time = 32 - minute;
-            if resources[3] + robots[3] * delta_time + (delta_time) * (delta_time - 1) / 2 <= max {
-                robots[robot] -= 1;
-                resources[0] += resources_per_robots[robot][0];
-                resources[1] += resources_per_robots[robot][1];
-                resources[2] += resources_per_robots[robot][2];
-                continue;
-            }
-            heap.push(Entry {
-                robots,
-                resources,
-                previously_built: true,
-                minute: minute + 1,
-            });
-            robots[robot] -= 1;
-            resources[0] += resources_per_robots[robot][0];
-            resources[1] += resources_per_robots[robot][1];
-            resources[2] += resources_per_robots[robot][2];
-        }
-        heap.push(Entry {
-            robots,
-            resources,
-            previously_built: false,
-            minute: minute + 1,
-        });
-    }
-    println!("{}", max);
-    max
+    // assert!(array.iter().all(|e| current_array.contains(e)));
+    let position_of_zero = current_array.iter().position(|&e| *e.1 == 0).unwrap();
+    let first = current_array[(position_of_zero + 1000) % array.len()].1;
+    let second = current_array[(position_of_zero + 2000) % array.len()].1;
+    let third = current_array[(position_of_zero + 3000) % array.len()].1;
+    println!("{}", first + second + third);
 }
