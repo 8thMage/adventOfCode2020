4a18fdfca513e833b5a064e9f47e6f8203757337
Eden Segal <Eee34353@gmail.com>
2024-07-27 10:12:43 +0300

    advent 2023: 5

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     env::current_exe,
     hash::Hash,
     iter,
-    ops::Neg,
+    ops::{Neg, RangeBounds},
     path,
     str::FromStr,
     thread::current,
@@ -13,71 +13,91 @@ use std::{
 
 fn main() {
     let input = include_str!("input.txt");
-    let sum: i64 = input
-        .lines()
-        .map(|line| {
-            let game = line.split_once(": ").unwrap().1;
-            let (winning, have) = game.split_once(" | ").unwrap();
-            let mut winning_hash = winning.split(" ").fold(HashSet::new(), |mut acc, s| {
-                if s != "" {
-                    acc.insert(i64::from_str(s).unwrap());
-                }
-                acc
-            });
-            have.split(" ").fold(0i64, |acc, s| {
-                if s == "" {
-                    return acc;
-                }
-                let num = i64::from_str(s).unwrap();
-                if winning_hash.contains(&num) {
-                    if acc == 0 {
-                        1i64
-                    } else {
-                        acc * 2
-                    }
-                } else {
-                    acc
-                }
-            })
+    let mut splitter = input.split("\n\n");
+    let seeds = splitter
+        .next()
+        .unwrap()
+        .split(" ")
+        .skip(1)
+        .map(|s| u64::from_str(s).unwrap());
+    let maps = splitter
+        .map(|s| {
+            s.lines()
+                .skip(1)
+                .map(|s| {
+                    s.split(" ")
+                        .map(|s| u64::from_str(s).unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
         })
-        .sum();
-    let winnings: Vec<_> = input
-        .lines()
-        .map(|line| {
-            let game = line.split_once(": ").unwrap().1;
-            let (winning, have) = game.split_once(" | ").unwrap();
-            let mut winning_hash = winning.split(" ").fold(HashSet::new(), |mut acc, s| {
-                if s != "" {
-                    acc.insert(i64::from_str(s).unwrap());
-                }
-                acc
-            });
-            winning_hash
-        })
-        .collect();
-    let mut multiplies = vec![1;input.lines().count()];
-    let new_sum:i64 = input
-        .lines().enumerate()
-        .map(|(index, line)| {
-            let game = line.split_once(": ").unwrap().1;
-            let (winning, have) = game.split_once(" | ").unwrap();
+        .collect::<Vec<_>>();
 
-            let sum = have.split(" ").filter(|s| {
-                if *s == "" {
-                    return false;
-                }
-                let num = i64::from_str(s).unwrap();
-                if winnings[index].contains(&num) {
-                    true
-                } else {
-                    false
-                }
-            }).count();
-            for d in 1..=sum {
-                multiplies[index+d]+=multiplies[index];
+    let min = seeds
+        .clone()
+        .map(|seed| {
+            let mut current = seed;
+            for map in &maps {
+                current = map
+                    .iter()
+                    .find(|s| (s[1]..s[1] + s[2]).contains(&current))
+                    .map_or(current, |s| s[0] + current - s[1]);
             }
-            multiplies[index]
-        }).sum();
-    println!("{}", sum);
-    println!("{}", new_sum);
+            // println!("{}", current);
+            current
+        })
+        .min()
+        .unwrap();
+    let mut splitter = input.split("\n\n");
+
+    let new_seeds = splitter
+        .next()
+        .unwrap()
+        .split(" ")
+        .skip(1)
+        .map(|s| u64::from_str(s).unwrap())
+        .collect::<Vec<_>>();
+    let mut new_seeds: Vec<[u64; 2]> = new_seeds
+        .windows(2)
+        .step_by(2)
+        .map(|s| [s[0], s[1]])
+        .collect::<Vec<_>>();
+    let first_sum = new_seeds.iter().map(|s| s[1]).sum::<u64>();
+    for map in &maps {
+        assert_eq!(new_seeds.iter().map(|s| s[1]).sum::<u64>(), first_sum);
+        let mut new_seeds_spliced = vec![];
+        for seed in &new_seeds {
+            let mut points_in_segment = vec![seed[0], seed[0] + seed[1]];
+            let mut found_map = false;
+            for specific_map in map {
+                if (seed[0]..seed[0] + seed[1]).contains(&specific_map[1]) {
+                    points_in_segment.push(specific_map[1]);
+                } else if (seed[0]..seed[0] + seed[1])
+                    .contains(&(specific_map[1] + specific_map[2]))
+                {
+                    points_in_segment.push(specific_map[1] + specific_map[2]);
+                }
+            }
+            points_in_segment.sort();
+            points_in_segment.dedup();
+            for window in points_in_segment.windows(2) {
+                new_seeds_spliced.push([window[0], window[1] - window[0]]);
+            }
+        }
+        assert_eq!(new_seeds_spliced.iter().map(|s| s[1]).sum::<u64>(), first_sum);
+        new_seeds.clear();
+        new_seeds = new_seeds_spliced
+            .iter()
+            .map(|s| {
+                map.iter()
+                    .find(|map| (map[1]..map[1] + map[2]).contains(&s[0]))
+                    .map_or(*s, |map| {
+                        [s[0] - map[1] + map[0], s[1]]
+                    })
+            })
+            .collect();
+        }
+
+    println!("{}", min);
+    println!("{}", new_seeds.iter().min().unwrap()[0]);
 }
