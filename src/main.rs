f23854c68fcfcd6e7f50ec3cb66e8be3eeece2ab
Eden Segal <Eee34353@gmail.com>
2024-07-27 20:26:03 +0300

    advent 2023: 9

--- src/main.rs
+++ src/main.rs
@@ -14,96 +14,29 @@ use std::{
 fn main() {
     let input = include_str!("input.txt");
     let mut lines = input.lines();
-    let mut order = lines
-        .next()
-        .unwrap()
-        .chars()
-        .map(|c| {
-            if c == 'R' {
-                1
-            } else if c == 'L' {
-                0
-            } else {
-                panic!()
-            }
-        })
-        .cycle();
-    lines.next();
-    let map = lines
-        .map(|line| {
-            let (key, pair) = line.split_once(" = (").unwrap();
-            let (left, right) = pair.split_once(", ").unwrap();
-            let right = right.split_once(")").unwrap().0;
-            (key, left, right)
-        })
-        .fold(HashMap::new(), |mut acc, (key, left, right)| {
-            acc.insert(key, (left, right));
-            acc
-        });
-    // let mut current = "AAA";
-    // let mut count = 0;
-    // while current != "ZZZ" {
-    //     let key = order.next().unwrap();
-    //     let pair = map[current];
-    //     if key == 0 {
-    //         current = pair.0;
-    //     } else {
-    //         current = pair.1;
-    //     }
-    //     count += 1;
-    // }
-    // println!("{}", count);
-    let mut lines = input.lines();
-    let mut order = lines
-        .next()
-        .unwrap()
-        .chars()
-        .map(|c| {
-            if c == 'R' {
-                1
-            } else if c == 'L' {
-                0
-            } else {
-                panic!()
-            }
-        })
-        .cycle();
-    let mut current = vec![];
-    for key in map.keys() {
-        if key.ends_with("A") {
-            current.push(*key);
+    let mut sum = 0;
+    let mut sum2 = 0;
+    for line in lines {
+        let mut deltas = vec![line
+            .split(" ")
+            .map(|s| i64::from_str(s).unwrap())
+            .collect::<Vec<_>>()];
+        while deltas.last().unwrap().iter().any(|x| *x != 0) {
+            deltas.push(
+                deltas
+                    .last()
+                    .unwrap()
+                    .windows(2)
+                    .map(|c| c[1] - c[0])
+                    .collect(),
+            );
         }
+        sum += deltas.iter().map(|arr| arr.last().unwrap()).sum::<i64>();
+        sum2 += deltas
+            .iter().rev()
+            .map(|arr| arr.first().unwrap())
+            .fold(0, |acc, x| x - acc);
     }
-    let mut count = 0;
-    let mut minhit = vec![u64::MAX; current.len()];
-    while !current.iter().all(|s| s.ends_with("Z")) {
-        let key = order.next().unwrap();
-        count += 1;
-        current.iter_mut().enumerate().for_each(|(index, current)| {
-            let pair = map[*current];
-            if key == 0 {
-                *current = pair.0;
-            } else {
-                *current = pair.1;
-            }
-            if current.ends_with("Z") {
-                minhit[index] = minhit[index].min(count);
-            }
-        });
-        if minhit.iter().all(|count| *count != u64::MAX) {
-            break;
-        }
-    }
-    let lcm = minhit.iter().fold(1, |acc, b| acc * *b / gcd(acc, *b));
-    println!("{}", lcm);
-}
-
-fn gcd(a: u64, b: u64) -> u64 {
-    if a < b {
-        return gcd(b, a);
-    }
-    if b == 0 {
-        return a
-    }
-    gcd(b, a.rem_euclid(b))
+    println!("{}", sum);
+    println!("{}", sum2);
 }
