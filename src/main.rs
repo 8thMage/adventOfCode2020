74810cc53e9d8d716490e37f8dba2e668b25554a
8thmage <>
2024-08-24 19:38:54 +0300

    advent 24: boxes of v

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
     collections::{BinaryHeap, HashMap, HashSet, VecDeque},
     fmt::Write,
     hash::Hash,
-    i64,
+    i128, i64,
     iter::{repeat, Cycle, FromIterator},
     mem::swap,
     rc::Rc,
@@ -25,7 +25,7 @@ struct Stone {
 
 #[derive(Clone, Copy, Debug)]
 struct StoneWindow {
-    start: Stone,
+    start: [i128; 3],
     size: u64,
 }
 
@@ -55,6 +55,7 @@ fn main() {
             let t = ((a.vx * (b.y - a.y) - a.vy * (b.x - a.x)) as f64)
                 / ((a.vy * b.vx - a.vx * b.vy) as f64);
             let s = (b.x as f64 - a.x as f64 + b.vx as f64 * t) / a.vx as f64;
+            // println!("0,  {} {}", t, s);
             if t < 0. || s < 0. {
                 continue;
             }
@@ -70,189 +71,320 @@ fn main() {
         }
     }
     println!("{}", count);
-    let mut stones = hail_stones
+    let min_x = hail_stones.iter().map(|stone| stone.x).min().unwrap();
+    let min_y = hail_stones.iter().map(|stone| stone.y).min().unwrap();
+    let min_z = hail_stones.iter().map(|stone| stone.z).min().unwrap();
+    let stones = hail_stones
         .iter()
         .map(|stone| {
             vec![
-                stone.x as f64 / 1e13,
-                stone.y as f64 / 1e13,
-                stone.z as f64 / 1e13,
-                stone.vx as f64,
-                stone.vy as f64,
-                stone.vz as f64,
+                (stone.x - min_x) as i128,
+                (stone.y - min_y) as i128,
+                (stone.z - min_z) as i128,
+                stone.vx as i128,
+                stone.vy as i128,
+                stone.vz as i128,
             ]
         })
         .collect::<Vec<_>>();
-    let mut current_min_value = 1e100;
-    let mut current_min = vec![];
-    for i in 0..10i32.pow(6) {
-        let a0 = i % 10;
-        let a1 = (i / 10) % 10;
-        let a2 = (i / 10i32.pow(2)) % 10;
-        let a3 = (i / 10i32.pow(3)) % 10;
-        let a4 = (i / 10i32.pow(4)) % 10;
-        let a5 = (i / 10i32.pow(5)) % 10;
-        let stone = vec![
-            50. * (a0 as f64 - 5.),
-            50. * (a1 as f64 - 5.),
-            50. * (a2 as f64 - 5.),
-            50. * (a3 as f64 - 5.),
-            50. * (a4 as f64 - 5.),
-            50. * (a5 as f64 - 5.),
-        ];
-        let v = calculate_current_function(&stone, &stones);
-        if current_min_value > v {
-            current_min_value = v;
-            current_min = stone.clone();
-        }
-    }
-    let mut current_stone = current_min;
-    let beta = 1. - 1e-4;
-    let mut current_gradient = vec![0.0; 6];
-    let mut index = 0;
+    let start_bits = 1 << 16 as u64;
+    let mut current_stage = vec![];
+    let mut next_stage = vec![];
+    // for dcoords in 0..1 << 3 {
+    //     let vx = (dcoords & 1) / 1;
+    //     let vy = (dcoords & 2) / 2;
+    //     let vz = (dcoords & 4) / 4;
+
+    //     current_stage.push(StoneWindow {
+    //         start: [
+    //             -vx * start_bits as i128,
+    //             -vy * start_bits as i128,
+    //             -vz * start_bits as i128,
+    //         ],
+    //         size: start_bits,
+    //     });
+    // }
+    println!(
+        "{}",
+        does_have_over_zero(
+            &StoneWindow {
+                start: [-20480 * 128, 65472 * 128, 2304 * 128],
+                size: 64 * 128
+            },
+            &stones
+        )
+    );
+
+    current_stage.push(StoneWindow {
+        start: [-20480 * 4, 65472 * 4, 2304 * 4],
+        size: 64*16,
+    });
+    current_stage.push(StoneWindow {
+        start: [20480 * 4, -65472 * 4, -2304 * 4],
+        size: 64*16,
+    });
     loop {
-        if index % 1 == 0 {
-            println!(
-                "index {} current_value {}",
-                index,
-                calculate_current_function(&current_stone, &stones).log2()
-            );
+        for (i, entry) in current_stage.drain(..).enumerate() {
+            if i % 1000 == 0 {
+                println!("heartbeat {}", i);
+            }
+            let new_size = if entry.size == 2 {
+                1
+            } else {
+                entry.size as i64 / 2
+            };
+            if entry.size == 0 {
+                // if gcd(
+                //     entry.start[0].abs(),
+                //     gcd(entry.start[1].abs(), entry.start[2].abs()),
+                // ) != 1
+                // {
+                //     continue;
+                // }
+                println!("{:?}", entry);
+                // println!("{:?}", entry.start.x + entry.start.y + entry.start.z);
+                // return;
+                continue;
+            }
+            for x_div in 0..2 {
+                let vx = entry.start[0] + x_div * new_size as i128;
+                if x_div >= entry.size as i128 {
+                    continue;
+                }
+
+                for y_div in 0..2 {
+                    let vy = entry.start[1] + y_div * new_size as i128;
+                    if y_div >= entry.size as i128 {
+                        continue;
+                    }
+                    for z_div in 0..2 {
+                        if z_div >= entry.size as i128 {
+                            continue;
+                        }
+                        let vz = entry.start[2] + z_div * new_size as i128;
+                        let next_stone_window = StoneWindow {
+                            start: [vx, vy, vz],
+                            size: new_size as u64,
+                        };
+                        if does_have_over_zero(&next_stone_window, &stones) {
+                            next_stage.push(next_stone_window);
+                        }
+                    }
+                }
+            }
         }
-        index += 1;
-        let delta_value_x =
-            calculate_delta_stone_x_coord(&current_stone, &stones, 0, 0, stones.len());
-        let delta_value_y =
-            calculate_delta_stone_x_coord(&current_stone, &stones, 1, 0, stones.len());
-        let delta_value_z =
-            calculate_delta_stone_x_coord(&current_stone, &stones, 2, 0, stones.len());
-        let delta_value_vx =
-            calculate_delta_stone_p_coord(&current_stone, &stones, 0, 0, stones.len());
-        let delta_value_vy =
-            calculate_delta_stone_p_coord(&current_stone, &stones, 1, 0, stones.len());
-        let delta_value_vz =
-            calculate_delta_stone_p_coord(&current_stone, &stones, 2, 0, stones.len());
-        let max = delta_value_x
-            .abs()
-            .max(delta_value_y.abs())
-            .max(delta_value_z.abs())
-            .max(delta_value_vx.abs())
-            .max(delta_value_vy.abs())
-            .max(delta_value_vz.abs())
-            / 10.;
-        for ((x, y), z) in current_gradient
-            .iter_mut()
-            .zip(current_stone.iter_mut())
-            .zip(
-                [
-                    delta_value_x,
-                    delta_value_y,
-                    delta_value_z,
-                    delta_value_vx,
-                    delta_value_vy,
-                    delta_value_vz,
-                ]
-                .iter(),
-            )
-        {
-            *x = *x * beta - (1. - beta) * z;
-            *y = *y + *x / 10.;
+
+        swap(&mut current_stage, &mut next_stage);
+        if current_stage[0].size == 4 {
+            for i in &current_stage {
+                println!("{:?}, {}, {}", i.start[0], i.start[1], i.start[2]);
+            }
+        }
+        println!("{:?} {}", current_stage[0].size, current_stage.len());
+    }
+
+    // for i in 0..hail_stones.len() {
+    //     for j in i + 1..hail_stones.len() {
+    //         let delta_x = stones[j][0] - stones[i][0];
+    //         let delta_y = stones[j][1] - stones[i][1];
+    //         let delta_z = stones[j][2] - stones[i][2];
+
+    //         let t = println!(
+    //             "1 {} {} {}",
+    //             triple_product(&[-3, 1, 2], &stones[i][3..6], &[delta_x, delta_y, delta_z]) as f64,
+    //             triple_product(&[-3, 1, 2], &stones[i][3..6], &[delta_x, delta_y, delta_z]) as f64
+    //                 / triple_product(&[0, 0, 1], &stones[j][3..6], &stones[i][3..6]) as f64,
+    //             triple_product(
+    //                 &[-3, 1, 2],
+    //                 &stones[j][3..6],
+    //                 &[-delta_x, -delta_y, -delta_z]
+    //             ) as f64
+    //                 / triple_product(&[0, 0, 1], &stones[i][3..6], &stones[j][3..6]) as f64
+    //         );
+    //     }
+    // }
+}
+
+fn does_have_over_zero(stone_window: &StoneWindow, hail_stones: &[Vec<i128>]) -> bool {
+    if stone_window.start == [0, 0, 0] && stone_window.size == 0 {
+        return false;
+    }
+    let mut min_x = f64::MIN;
+    let mut max_x = f64::MAX;
+    let mut min_y = f64::MIN;
+    let mut max_y = f64::MAX;
+    for i in 0..hail_stones.len() {
+        for j in i + 1..hail_stones.len() {
+            let mut min_0_0 = i128::MAX;
+            let mut max_0_0 = i128::MIN;
+            let mut min_0_1 = i128::MAX;
+            let mut max_0_1 = i128::MIN;
+
+            let mut min_1_0 = i128::MAX;
+            let mut max_1_0 = i128::MIN;
+            let mut min_1_1 = i128::MAX;
+            let mut max_1_1 = i128::MIN;
+
+            let delta_x = hail_stones[j][0] - hail_stones[i][0];
+            let delta_y = hail_stones[j][1] - hail_stones[i][1];
+            let delta_z = hail_stones[j][2] - hail_stones[i][2];
+            for dcoords in 0..(1 << 3).min(1 + stone_window.size as i128 * 8) {
+                let mut v = [
+                    stone_window.start[0] + (dcoords & 1) * stone_window.size as i128,
+                    stone_window.start[1] + ((dcoords & 2) / 2) * stone_window.size as i128,
+                    stone_window.start[2] + ((dcoords & 4) / 4) * stone_window.size as i128,
+                ];
+                let triple_0 =
+                    triple_product(&v, &hail_stones[i][3..6], &[delta_x, delta_y, delta_z]);
+                max_0_0 = max_0_0.max(triple_0);
+                min_0_0 = min_0_0.min(triple_0);
+                let triple_1 = triple_product(&v, &hail_stones[j][3..6], &hail_stones[i][3..6]);
+                max_0_1 = max_0_1.max(triple_1);
+                min_0_1 = min_0_1.min(triple_1);
+
+                let triple_2 =
+                    triple_product(&v, &hail_stones[j][3..6], &[-delta_x, -delta_y, -delta_z]);
+                max_1_0 = max_1_0.max(triple_2);
+                min_1_0 = min_1_0.min(triple_2);
+                max_1_1 = max_1_1.max(-triple_1);
+                min_1_1 = min_1_1.min(-triple_1);
+            }
+            let is_negative = (min_0_0 > 0 && max_0_1 < 0)
+                || (min_0_1 > 0 && max_0_0 < 0)
+                || (min_1_0 > 0 && max_1_1 < 0)
+                || (min_1_1 > 0 && max_1_0 < 0)
+                || ((min_0_0 == 0 && max_0_0 == 0) && (min_0_1 > 0 || max_0_1 < 0))
+                || ((min_1_0 == 0 && max_1_0 == 0) && (min_1_1 > 0 || max_1_1 < 0));
+            if is_negative {
+                return false;
+            }
+            if min_0_1 <= 0 && max_0_1 >= 0 {
+                continue;
+            }
+            let a = (max_0_0 as f64 / min_0_1 as f64).max(0.0);
+            let b = (min_0_0 as f64 / max_0_1 as f64).max(0.0);
+            let max_t = a.max(b);
+            let min_t = a.min(b);
+            let max_pos_0 = [
+                hail_stones[j][0] as f64 + hail_stones[j][3] as f64 * max_t,
+                hail_stones[j][1] as f64 + hail_stones[j][4] as f64 * max_t,
+                hail_stones[j][2] as f64 + hail_stones[j][5] as f64 * max_t,
+            ];
+            let min_pos_0 = [
+                hail_stones[j][0] as f64 + hail_stones[j][3] as f64 * min_t,
+                hail_stones[j][1] as f64 + hail_stones[j][4] as f64 * min_t,
+                hail_stones[j][2] as f64 + hail_stones[j][5] as f64 * min_t,
+            ];
+
+            let a = (max_1_0 as f64 / min_1_1 as f64).max(0.0);
+            let b = (min_1_0 as f64 / max_1_1 as f64).max(0.0);
+            let max_t = a.max(b);
+            let min_t = a.min(b);
+            let max_pos_1 = [
+                hail_stones[i][0] as f64 + hail_stones[i][3] as f64 * max_t,
+                hail_stones[i][1] as f64 + hail_stones[i][4] as f64 * max_t,
+                hail_stones[i][2] as f64 + hail_stones[i][5] as f64 * max_t,
+            ];
+            let min_pos_1 = [
+                hail_stones[i][0] as f64 + hail_stones[i][3] as f64 * min_t,
+                hail_stones[i][1] as f64 + hail_stones[i][4] as f64 * min_t,
+                hail_stones[i][2] as f64 + hail_stones[i][5] as f64 * min_t,
+            ];
+            let mut current_max_0_x = f64::MIN;
+            let mut current_min_0_x = f64::MAX;
+            let mut current_max_0_y = f64::MIN;
+            let mut current_min_0_y = f64::MAX;
+
+            let mut current_max_1_x = f64::MIN;
+            let mut current_min_1_x = f64::MAX;
+            let mut current_max_1_y = f64::MIN;
+            let mut current_min_1_y = f64::MAX;
+            for dcoords in 0..(1 << 3).min(1 + stone_window.size as i128 * 8) {
+                let v = [
+                    stone_window.start[0] as f64
+                        + ((dcoords & 1) * stone_window.size as i128) as f64,
+                    stone_window.start[1] as f64
+                        + (((dcoords & 2) / 2) * stone_window.size as i128) as f64,
+                    stone_window.start[2] as f64
+                        + (((dcoords & 4) / 4) * stone_window.size as i128) as f64,
+                ];
+                let cross_x = cross_product_x(&v, &max_pos_0);
+                current_max_0_x = current_max_0_x.max(cross_x);
+                current_min_0_x = current_min_0_x.min(cross_x);
+                let cross_x = cross_product_x(&v, &min_pos_0);
+                current_max_0_x = current_max_0_x.max(cross_x);
+                current_min_0_x = current_min_0_x.min(cross_x);
+
+                let cross_y = cross_product_y(&v, &max_pos_0);
+                current_max_0_y = current_max_0_y.max(cross_y);
+                current_min_0_y = current_min_0_y.min(cross_y);
+                let cross_y = cross_product_y(&v, &min_pos_0);
+                current_max_0_y = current_max_0_y.max(cross_y);
+                current_min_0_y = current_min_0_y.min(cross_y);
+
+                let cross_x = cross_product_x(&v, &max_pos_1);
+                current_max_1_x = current_max_1_x.max(cross_x);
+                current_min_1_x = current_min_1_x.min(cross_x);
+                let cross_x = cross_product_x(&v, &min_pos_1);
+                current_max_1_x = current_max_1_x.max(cross_x);
+                current_min_1_x = current_min_1_x.min(cross_x);
+
+                let cross_y = cross_product_y(&v, &max_pos_1);
+                current_max_1_y = current_max_1_y.max(cross_y);
+                current_min_1_y = current_min_1_y.min(cross_y);
+                let cross_y = cross_product_y(&v, &min_pos_1);
+                current_max_1_y = current_max_1_y.max(cross_y);
+                current_min_1_y = current_min_1_y.min(cross_y);
+            }
+            if current_max_0_x < min_x
+                || current_min_0_x > max_x
+                || current_max_1_x < min_x
+                || current_min_1_x > max_x
+            {
+                return false;
+            }
+            max_x = max_x.min(current_max_0_x).min(current_max_1_x);
+            min_x = min_x.max(current_min_0_x).max(current_min_1_x);
+
+            if current_max_0_y < min_y
+                || current_min_0_y > max_y
+                || current_max_1_y < min_y
+                || current_min_1_y > max_y
+            {
+                return false;
+            }
+            max_y = max_y.min(current_max_0_y).min(current_max_1_y);
+            min_y = min_y.max(current_min_0_y).max(current_min_1_y);
         }
     }
+    return true;
 }
 
-fn calculate_current_function(current_stone: &Vec<f64>, stones: &[Vec<f64>]) -> f64 {
-    let value = stones
-        .iter()
-        .map(|stone| {
-            let diff = current_stone
-                .iter()
-                .zip(stone.iter())
-                .map(|(x, y)| x - y)
-                .collect::<Vec<_>>();
-            (diff[0] * diff[4]).abs()
-                + (diff[0] * diff[5]).abs()
-                + (diff[1] * diff[3]).abs()
-                + (diff[1] * diff[5]).abs()
-                + (diff[2] * diff[3]).abs()
-                + (diff[2] * diff[4]).abs()
-        })
-        .sum::<f64>();
-    return value;
+fn triple_product(a: &[i128], b: &[i128], c: &[i128]) -> i128 {
+    let mut sum = 0;
+    for i in 0..3 {
+        sum += a[i] * b[(i + 1) % 3] * c[(i + 2) % 3];
+    }
+    for i in 0..3 {
+        sum -= a[i] * b[(i + 2) % 3] * c[(i + 1) % 3];
+    }
+    sum as i128
 }
 
-fn calculate_index_of_max(current_stone: &Vec<f64>, stones: &[Vec<f64>]) -> usize {
-    let value = stones
-        .iter()
-        .map(|stone| {
-            let diff = current_stone
-                .iter()
-                .zip(stone.iter())
-                .map(|(x, y)| x - y)
-                .collect::<Vec<_>>();
-            (diff[0] * diff[4]).abs()
-                + (diff[0] * diff[5]).abs()
-                + (diff[1] * diff[3]).abs()
-                + (diff[1] * diff[5]).abs()
-                + (diff[2] * diff[3]).abs()
-                + (diff[2] * diff[4]).abs()
-        })
-        .enumerate()
-        .max_by_key(|(i, x)| *x as i64);
-    return value.unwrap().0;
+fn cross_product_x(a: &[f64], b: &[f64]) -> f64 {
+    a[1] * b[2] - a[2] * b[1]
 }
 
-fn calculate_delta_stone_x_coord(
-    current_stone: &Vec<f64>,
-    stones: &[Vec<f64>],
-    coord: usize,
-    start_index: usize,
-    len: usize,
-) -> f64 {
-    let delta_coord_1 = (coord + 1) % 3;
-    let delta_coord_2 = (coord + 2) % 3;
-    let index = calculate_index_of_max(current_stone, stones);
-    let grad = stones
-        .iter()
-        .cycle()
-        .skip(index)
-        .take(len)
-        .map(|stone| {
-            let diff = current_stone
-                .iter()
-                .zip(stone.iter())
-                .map(|(x, y)| x - y)
-                .collect::<Vec<_>>();
-            diff[coord].signum() * (diff[delta_coord_1 + 3].abs() + diff[delta_coord_2 + 3].abs())
-        })
-        .sum::<f64>();
-    return grad;
+fn cross_product_y(a: &[f64], b: &[f64]) -> f64 {
+    a[2] * b[0] - a[0] * b[2]
 }
 
-fn calculate_delta_stone_p_coord(
-    current_stone: &Vec<f64>,
-    stones: &[Vec<f64>],
-    coord: usize,
-    start_index: usize,
-
-    len: usize,
-) -> f64 {
-    let coord = coord + 3;
-    let delta_coord_1 = ((coord + 1) % 3) + 3;
-    let delta_coord_2 = ((coord + 2) % 3) + 3;
-    let index = calculate_index_of_max(current_stone, stones);
-    let grad = stones
-        .iter()
-        .cycle()
-        .skip(index)
-        .take(len)
-        .map(|stone| {
-            let diff = current_stone
-                .iter()
-                .zip(stone.iter())
-                .map(|(x, y)| x - y)
-                .collect::<Vec<_>>();
-            diff[coord].signum() * (diff[delta_coord_1 - 3].abs() + diff[delta_coord_2 - 3].abs())
-        })
-        .sum::<f64>();
-    return grad;
+fn gcd(a: i128, b: i128) -> i128 {
+    if b == 0 {
+        return a;
+    }
+    if a < b {
+        return gcd(b, a);
+    }
+    return gcd(b, a.rem_euclid(b));
 }
