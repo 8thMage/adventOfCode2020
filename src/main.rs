48dfcff9f57168bd127fc45590176472dd349858
Eden Segal <eden.segal@nextsilicon.com>
2025-01-05 22:27:53 +0200

    advent 2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 fn main() {
     let input = include_str!("input.txt");
-    let mut first = vec![];
-    let mut second = vec![];
-    for line in input.lines() {
-        let mut split = line.split_whitespace();
-        first.push(i64::from_str_radix(split.next().unwrap(), 10).unwrap());
-        second.push(i64::from_str_radix(split.next().unwrap(), 10).unwrap());
+    let mut sum = 0;
+    'next: for line in input.lines() {
+        'i: for i in 0..14 {
+            let mut is_increasing = true;
+            let mut is_decreasing = true;
+            let split = line.split_whitespace().map(|x| x.parse::<u64>().unwrap());
+            let split3 = split.clone();
+            let split = split.take(i).chain(split3.skip(i + 1));
+            let split2 = split.clone().skip(1);
+            for (a, b) in split.zip(split2) {
+                if a > b {
+                    is_increasing = false;
+                    if !is_decreasing {
+                        continue 'i;
+                    }
+                }
+                if a < b {
+                    is_decreasing = false;
+                    if !is_increasing {
+                        continue 'i;
+                    }
+                }
+                if a.abs_diff(b) < 1 || a.abs_diff(b) > 3 {
+                    continue 'i;
+                }
+            }
+            if is_increasing || is_decreasing {
+                sum += 1;
+                continue 'next;
+            }
+        }
     }
-    first.sort();
-    second.sort();
-    let res: u64 = first
-        .iter()
-        .zip(second.iter())
-        .map(|(a, b)| a.abs_diff(*b))
-        .sum();
-    let res2: usize = first
-        .iter()
-        .map(|&a| (a as usize) * second.iter().filter(|&&b| b == a).count())
-        .sum();
-    println!("{}", res);
-    println!("{}", res2);
+    println!("{}", sum);
 }
